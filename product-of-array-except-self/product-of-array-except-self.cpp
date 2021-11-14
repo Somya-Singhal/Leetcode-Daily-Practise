f13ddb4a71c12761a08e02b75685ce0bca348ea7
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-11-14 18:11:04 +0530

    Time: 16 ms (97.18%), Space: 23.7 MB (99.50%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    //beauty->use suffix and prefix array
+    // without extra space
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int>ans(n);
-        int pre_product[n],pre_suffix[n];
-        pre_product[0]=nums[0];
-        pre_suffix[n-1]=nums[n-1];
+        vector<int>res(n);
+        res[0]=1;
         for(int i=1;i<n;i++)
-            pre_product[i]=pre_product[i-1]*nums[i];
-        for(int i=n-2;i>=0;i--)
-            pre_suffix[i]=pre_suffix[i+1]*nums[i];
-        for(int i=0;i<n;i++)
         {
-            ans[i]=((i>=1)?pre_product[i-1]:1)*((i<n-1)?pre_suffix[i+1]:1);
+            res[i]=res[i-1]*nums[i-1];
         }
-        return ans;
+        int right=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            res[i]*=right;
+            right*=nums[i];
+        }
+        return res;
     }
 };
