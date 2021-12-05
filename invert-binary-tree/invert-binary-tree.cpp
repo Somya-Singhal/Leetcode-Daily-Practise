6c1c45851e745a464894bd3421d3e78906444d6e
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-06 00:01:59 +0530

    Update

--- invert-binary-tree/invert-binary-tree.cpp
+++ invert-binary-tree/invert-binary-tree.cpp
@@ -11,12 +11,31 @@
  */
 class Solution {
 public:
+//     TreeNode* invertTree(TreeNode* root) {
+//         if(root==NULL)
+//             return root;
+//         TreeNode* temp=root->left;
+//         root->left=invertTree(root->right);
+//         root->right=invertTree(temp);
+//         return root;
+//     }
+    
+    // same solution can be written using stack also
     TreeNode* invertTree(TreeNode* root) {
         if(root==NULL)
             return root;
-        TreeNode* temp=root->left;
-        root->left=invertTree(root->right);
-        root->right=invertTree(temp);
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* curr=q.front();
+            q.pop();
+            TreeNode* tmp=curr->left;
+            curr->left=curr->right;
+            curr->right=tmp;
+            if(curr->left) q.push(curr->left);
+            if(curr->right) q.push(curr->right);
+        }
         return root;
     }
 };
