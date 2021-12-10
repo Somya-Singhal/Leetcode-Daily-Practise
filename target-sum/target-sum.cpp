37e011f4dba9815ad0353af35a5e5e93c4b47f8c
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-10 22:27:28 +0530

    Update

--- target-sum/target-sum.cpp
+++ target-sum/target-sum.cpp
@@ -60,49 +60,44 @@ public:
     //     int val=(target+sum)/2;
     //    return countsubsetsum(0,nums,val);
     // }
-    int countSubsets(vector<int>& nums, int n, int M)
+    int countsubsetsum(int idx,vector<int>& nums, int val)
     {
-        int t[n + 1][M + 1];
-        
-       for(int i = 0; i <= n; i++)
+        int n=nums.size();
+        int dp[n+1][val+1];
+        for(int i=0;i<=n;i++)
         {
-            for(int j = 0; j <= M; j++)
+            for(int j=0;j<=val;j++)
             {
-                if(i == 0)
-                    t[i][j] = 0;
-                if(j == 0)
-                    t[i][j] = 1;
+                if(i==0)
+                    dp[i][j]=0;
+                if(j==0)
+                    dp[i][j]=1;
             }
         }
-        
-        //t[0][0] = 1;
-        
-        for(int i = 1; i <= n; i++)
+        for(int i=1;i<=n;i++)
         {
-            for(int j = 0; j <= M; j++)
+            for(int j=0;j<=val;j++)
             {
-                if(nums[i - 1] <= j)
-                 t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
+                if(nums[i-1]<=j)
+                    dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
                 else
-                  t[i][j] = t[i - 1][j];
+                    dp[i][j]=dp[i-1][j];
             }
         }
-        
-        return t[n][M];  
+        return dp[n][val];
     }
-    
-    int findTargetSumWays(vector<int>& nums, int target)
-     {
-         target = abs(target);
-         int n = nums.size();
-         int sum = 0;
-         for(int i = 0; i < n; i++)
-             sum += nums[i];
-         
-        int M = (sum + target)/2;
-        if(sum < target || (sum + target) % 2 != 0)
+    int findTargetSumWays(vector<int>& nums, int target) {
+        target=abs(target);
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+        }
+        if((target+sum)%2!=0 || target>sum)
             return 0;
-         return countSubsets(nums, n, M);
-     }
+        int val=(target+sum)/2;
+       return countsubsetsum(0,nums,val);
+    }
+       
        
 };
