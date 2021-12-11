58b5679329896058b3a711b4f9b686a58b6a93b4
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-12 00:50:33 +0530

    Update

--- longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    //recursive
+//     int LISrec(vector<int>& nums,int n,int *ans)
+//     {
+//        if(n==1)
+//             return 1;
+//         int res,max_ending=1;
+//        for(int i=1;i<n;i++)
+//        {
+//            res=LISrec(nums,i,ans);
+//            if(nums[i-1]<nums[n-1] && res+1>max_ending)
+//                max_ending=res+1;
+//        }
+//         if(*ans<max_ending)
+//             *ans=max_ending;
+        
+//         return max_ending;
+//     }
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+//         int ans=1;
+//        LISrec(nums,n,&ans);
+//         return ans;
+//     }
+    
+    //dp
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         int dp[n];
