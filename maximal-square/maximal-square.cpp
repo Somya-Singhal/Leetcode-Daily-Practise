91b504636947cff05584a5b35801baa29be5cd82
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-14 11:33:55 +0530

    Update

--- maximal-square/maximal-square.cpp
+++ maximal-square/maximal-square.cpp
@@ -41,21 +41,44 @@ public:
 //         return maxside_len*maxside_len;
 //     }
 //     dp-->O(n*m)
-    int maximalSquare(vector<vector<char>>& matrix) {
+//     int maximalSquare(vector<vector<char>>& matrix) {
+//         int n=matrix.size();
+//         int m=matrix[0].size();
+//         int ans=0;
+//         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+//         for(int i=1;i<=n;i++)
+//         {
+//             for(int j=1;j<=m;j++)
+//             {
+                
+//                 if(matrix[i-1][j-1]=='1')
+//                 {
+//                     dp[i][j]=min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
+//                     ans=max(ans,dp[i][j]);
+//                 }  
+//             }
+//         }
+//         return ans*ans;
+//     }
+    //     dp-->space-O(m)
+     int maximalSquare(vector<vector<char>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        int ans=0;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+         vector<int>dp(m+1,0);
+        int prev=0,ans=0;
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                
+                int temp=dp[j];
                 if(matrix[i-1][j-1]=='1')
                 {
-                    dp[i][j]=min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
-                    ans=max(ans,dp[i][j]);
-                }  
+                    dp[j]=min(min(dp[j],dp[j-1]),prev)+1;
+                    ans=max(ans,dp[j]);
+                } 
+                else
+                    dp[j]=0;
+                prev=temp;
             }
         }
         return ans*ans;
