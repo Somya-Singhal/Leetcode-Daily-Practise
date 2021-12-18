1b400aff828759241d666797ff5a6b0194be5459
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-18 11:07:47 +0530

    Update

--- binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -11,6 +11,21 @@
  */
 class Solution {
 public:
+//     int PathSumrec(TreeNode* root,int *maxSum)
+//     {
+//         if(root==NULL)
+//             return 0;
+//         int left=max(PathSumrec(root->left,maxSum),0);
+//         int right=max(PathSumrec(root->right,maxSum),0);
+//         int currpathsum=root->val+left+right;
+//         *maxSum=max(*maxSum,currpathsum);
+//         return root->val+max(left,right);
+//     }
+//     int maxPathSum(TreeNode* root) {
+//         int maxSum=INT_MIN;
+//         PathSumrec(root,&maxSum);
+//         return maxSum;
+//     }
     int maxpathsum=INT_MIN;
     int PathSumrec(TreeNode* root)
     {
