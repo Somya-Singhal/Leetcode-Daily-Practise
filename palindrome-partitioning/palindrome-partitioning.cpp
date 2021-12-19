4a3edbbff6ae959e6d14a3ef34dcc53ccf97957a
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-19 22:47:12 +0530

    Time: 88 ms (89.05%), Space: 53.3 MB (74.32%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.cpp
+++ palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,30 +1,59 @@
 class Solution {
 public:
-    bool isvalid(int i,int j,string s)
-    {
-        while(i<=j)
-        {
-          if(s[i]!=s[j])
-              return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    void partitionrec(int i,int j,string s,vector<string>& v,vector<vector<string>>& res)
+    
+    //recursive--->(n*2^n) Space->O(n)
+    // bool isvalid(int i,int j,string s)
+    // {
+    //     while(i<=j)
+    //     {
+    //       if(s[i]!=s[j])
+    //           return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+    // void partitionrec(int i,int j,string s,vector<string>& v,vector<vector<string>>& res)
+    // {
+    //     if(i==s.length())
+    //     {
+    //          res.push_back(v);
+    //          return;
+    //     }
+    //     for(int k=i;k<j;k++)
+    //     {
+    //         if(isvalid(i,k,s))
+    //         {
+    //             v.push_back(s.substr(i,k-i+1));
+    //             partitionrec(k+1,j,s,v,res); 
+    //             v.pop_back();
+    //         }
+    //     }
+    //     return;
+    // }
+    // vector<vector<string>> partition(string s) {
+    //     vector<vector<string>>res;
+    //     vector<string>v;
+    //     int n=s.length();
+    //     partitionrec(0,n,s,v,res);
+    //     return res;
+    // }
+    
+    //dp
+    void partitionrec(int i,int j,string s,vector<string>& v,vector<vector<string>>& res,vector<vector<bool>>& dp)
     {
         if(i==s.length())
         {
              res.push_back(v);
-             // v.clear();
              return;
         }
         for(int k=i;k<j;k++)
         {
-            if(isvalid(i,k,s))
+            if(s[i]==s[k] && (k-i<=2 || dp[i+1][k-1]))
             {
+                dp[i][k]=true;
                 v.push_back(s.substr(i,k-i+1));
-                partitionrec(k+1,j,s,v,res); 
+                partitionrec(k+1,j,s,v,res,dp); 
                 v.pop_back();
             }
         }
@@ -34,7 +63,8 @@ public:
         vector<vector<string>>res;
         vector<string>v;
         int n=s.length();
-        partitionrec(0,n,s,v,res);
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        partitionrec(0,n,s,v,res,dp);
         return res;
     }
 };
