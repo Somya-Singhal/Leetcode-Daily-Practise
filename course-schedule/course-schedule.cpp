61085f9bcd1cf74e1ab3f1261901470f0cf8ee27
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-21 22:27:05 +0530

    Update

--- course-schedule/course-schedule.cpp
+++ course-schedule/course-schedule.cpp
@@ -1,44 +1,83 @@
 class Solution {
 public:
-    //find all nodes can be visited or not
-    bool dfs(int i,vector<bool>& visited,vector<bool>& rectst,vector<vector<int>>& adj)
-    {
-        visited[i]=true;
-        rectst[i]=true;
-        for(auto x:adj[i])
-        {
-            if(!visited[x] && dfs(x,visited,rectst,adj))
-                return true;
-            else if(rectst[x]==true)
-                return true;
-        }
-        rectst[i]=false;
-        return false;
-    }
+   //detect cycle in a directed graph
+    
+    //dfs approach
+    // bool dfs(int i,vector<bool>& visited,vector<bool>& rectst,vector<vector<int>>& adj)
+    // {
+    //     visited[i]=true;
+    //     rectst[i]=true;
+    //     for(auto x:adj[i])
+    //     {
+    //         if(!visited[x] && dfs(x,visited,rectst,adj))
+    //             return true;
+    //         else if(rectst[x]==true)
+    //             return true;
+    //     }
+    //     rectst[i]=false;
+    //     return false;
+    // }
+    // bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    //     if(prerequisites.size()==0)
+    //         return true;
+    //     vector<bool>visited(numCourses,false);
+    //     vector<bool>rectst(numCourses,false);
+    //     vector<vector<int>>adj(numCourses,vector<int>());
+    //     int max_outdegree=0;
+    //     for(auto x:prerequisites)
+    //     {
+    //         int u=x[0];
+    //         int v=x[1];
+    //         if(u==v)
+    //             return false;
+    //         adj[u].push_back(v);
+    //     }
+    //     for(int i=0;i<numCourses;i++)
+    //     {
+    //         if(!visited[i] && dfs(i,visited,rectst,adj)==true)
+    //           return false;
+    //     }
+    //     return true;
+    // }
+    
+    //kahn's algorithm used
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         if(prerequisites.size()==0)
             return true;
-        // vector<int>outdegree(numCourses,0);
+        vector<bool>visited(numCourses,false);
+        vector<int>indegree(numCourses,0);
         vector<vector<int>>adj(numCourses,vector<int>());
-        int max_outdegree=0;
+        queue<int>q;
+        int count=0;
         for(auto x:prerequisites)
         {
             int u=x[0];
             int v=x[1];
             if(u==v)
                 return false;
-            // outdegree[u]++;
-            // if(outdegree[u]>outdegree[max_outdegree])
-            //   max_outdegree=u;
+            indegree[v]++;
             adj[u].push_back(v);
         }
         for(int i=0;i<numCourses;i++)
         {
-            vector<bool>visited(numCourses,false);
-            vector<bool>rectst(numCourses,false);
-            if(dfs(i,visited,rectst,adj)==true)
-              return false;
+            if(!indegree[i])
+                q.push(i);
         }
-        return true;
+        while(!q.empty())
+        {
+            int u=q.front();
+            q.pop();
+            for(auto v:adj[u])
+            {
+                if(!visited[v])
+                {
+                    indegree[v]--;
+                    if(!indegree[v])
+                        q.push(v);
+                }
+            }
+            count++;
+        }
+        return count==numCourses;
     }
 };
