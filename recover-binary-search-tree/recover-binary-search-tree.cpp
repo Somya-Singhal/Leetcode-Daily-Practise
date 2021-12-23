45ae78715318a175f356c60a14ce4d671f28b636
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-23 11:47:48 +0530

    Update

--- recover-binary-search-tree/recover-binary-search-tree.cpp
+++ recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -12,27 +12,57 @@
 class Solution {
 public:
     void recoverTree(TreeNode* root) {
-        TreeNode* first=NULL,*second=NULL,*curr=root,*prev=NULL;
-        stack<TreeNode*>s;
-        while(curr || !s.empty())
+        //stack used->O(n)
+    // void recoverTree(TreeNode* root) {
+    //     TreeNode* first=NULL,*second=NULL,*curr=root,*prev=NULL;
+    //     stack<TreeNode*>s;
+    //     while(curr || !s.empty())
+    //     {
+    //         while(curr!=NULL)
+    //         {
+    //            s.push(curr);
+    //            curr=curr->left;
+    //         }
+    //         curr=s.top();
+    //         s.pop();
+    //         if(first!=NULL && second!=NULL && first->val>curr->val)
+    //             second=curr;
+    //         else if(prev!=NULL && prev->val>curr->val)
+    //         {
+    //             first=prev;
+    //             second=curr;
+    //         }
+    //         prev=curr;
+    //         curr=curr->right;
+    //     }
+    //     int temp=first->val;
+    //     first->val=second->val;
+    //     second->val=temp;
+    //     return;
+    // }
+    
+    //recursive inorder
+    TreeNode* prev=NULL,*first=NULL,*second=NULL;
+    void inorder(TreeNode* root)
+    {
+        if(root==NULL)
+            return;
+        inorder(root->left);
+        if(first!=NULL && second!=NULL && first->val>root->val)
+                second=root;
+        else if(prev && prev->val>root->val)
         {
-            while(curr!=NULL)
-            {
-               s.push(curr);
-               curr=curr->left;
-            }
-            curr=s.top();
-            s.pop();
-            if(first!=NULL && second!=NULL && first->val>curr->val)
-                second=curr;
-            else if(prev!=NULL && prev->val>curr->val)
-            {
-                first=prev;
-                second=curr;
-            }
-            prev=curr;
-            curr=curr->right;
+            first=prev;
+            second=root;
         }
+        prev=root;
+        inorder(root->right);
+        return;
+    }
+    void recoverTree(TreeNode* root) {
+        if(root==NULL)
+            return;
+        inorder(root);
         int temp=first->val;
         first->val=second->val;
         second->val=temp;
