9a168b7503d045d5684dfa4fc8a6bac6f17bd73d
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-27 01:02:21 +0530

    Time: 20 ms (28.13%), Space: 32.6 MB (41.47%) - LeetHub

--- path-sum-ii/path-sum-ii.cpp
+++ path-sum-ii/path-sum-ii.cpp
@@ -11,29 +11,64 @@
  */
 class Solution {
 public:
-    vector<vector<int>>res;
-    void pathsumrec(TreeNode* root,int targetSum,vector<int>& ans)
-    {
-        if(root==NULL)
-            return;
-        if(root->left==NULL && root->right==NULL && targetSum==root->val)
-        {
-            ans.push_back(root->val);
-            res.push_back(ans);
-            ans.pop_back();
-            return;
-        }
-        if(root->left==NULL && root->right==NULL)
-        return;
-        ans.push_back(root->val);
-        pathsumrec(root->left,targetSum-root->val,ans);
-        pathsumrec(root->right,targetSum-root->val,ans);
-        ans.pop_back();
-        return;    
-    }
+    //recursion
+    // vector<vector<int>>res;
+    // void pathsumrec(TreeNode* root,int targetSum,vector<int>& ans)
+    // {
+    //     if(root==NULL)
+    //         return;
+    //     if(root->left==NULL && root->right==NULL && targetSum==root->val)
+    //     {
+    //         ans.push_back(root->val);
+    //         res.push_back(ans);
+    //         ans.pop_back();
+    //         return;
+    //     }
+    //     if(root->left==NULL && root->right==NULL)
+    //     return;
+    //     ans.push_back(root->val);
+    //     pathsumrec(root->left,targetSum-root->val,ans);
+    //     pathsumrec(root->right,targetSum-root->val,ans);
+    //     ans.pop_back();
+    //     return;    
+    // }
+    // vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+    //     vector<int>ans;
+    //    pathsumrec(root,targetSum,ans);
+    //     return res;
+    // }
+    
+    //queue or stack(same implementation)
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+       vector<vector<int>>res;
+        if(root==NULL)
+            return res;
+        queue<pair<pair<TreeNode*,int>,vector<int>>>q;
         vector<int>ans;
-       pathsumrec(root,targetSum,ans);
+        ans.push_back(root->val);
+        q.push(make_pair(make_pair(root,root->val),ans));
+        while(!q.empty())
+        {
+            pair<TreeNode*,int>p1;
+            p1=q.front().first;
+            vector<int>ans=q.front().second;
+            TreeNode* curr=p1.first;
+            int val=p1.second;
+            q.pop();
+            if(curr->left==NULL && curr->right==NULL && val==targetSum)
+                res.push_back(ans);
+            if(curr->left)
+            {
+                ans.push_back(curr->left->val);
+                q.push(make_pair(make_pair(curr->left,val+curr->left->val),ans));
+                ans.pop_back();
+            }
+            if(curr->right)
+            {
+                ans.push_back(curr->right->val);
+                q.push(make_pair(make_pair(curr->right,val+curr->right->val),ans));
+            }
+        }
         return res;
     }
 };
