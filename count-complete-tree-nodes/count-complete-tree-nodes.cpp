d468b564cc9263b2565f8be7a34ca5ebc60b0f50
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-28 02:38:55 +0530

    Update

--- count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,28 +11,54 @@
  */
 class Solution {
 public:
-    int leftdist(TreeNode* root)
+    // int leftdist(TreeNode* root)
+    // {
+    //     if(root==NULL)
+    //         return 0;
+    //     return 1+leftdist(root->left);
+    // }
+    // int rightdist(TreeNode* root)
+    // {
+    //     if(root==NULL)
+    //         return 0;
+    //     return 1+rightdist(root->right);
+    // }
+    // int countNodes(TreeNode* root) {
+    //     if(root!=NULL)
+    //     {
+    //         int left=leftdist(root->left);
+    //         int right=rightdist(root->right);
+    //         if(left==right)
+    //             return pow(2,left+1)-1;
+    //         else 
+    //             return 1+countNodes(root->left)+countNodes(root->right);
+    //     }
+    //     return 0;
+    // }
+    
+    //time->O((logn)^2)
+    int leftheight(TreeNode* root)
     {
         if(root==NULL)
             return 0;
-        return 1+leftdist(root->left);
-    }
-    int rightdist(TreeNode* root)
-    {
-        if(root==NULL)
-            return 0;
-        return 1+rightdist(root->right);
+        return 1+leftheight(root->left);
     }
     int countNodes(TreeNode* root) {
-        if(root!=NULL)
+        int nodes=0,h=leftheight(root);
+        while(root!=NULL)
         {
-            int left=leftdist(root->left);
-            int right=rightdist(root->right);
-            if(left==right)
-                return pow(2,left+1)-1;
-            else 
-                return 1+countNodes(root->left)+countNodes(root->right);
+            if(leftheight(root->right)==h-1)
+            {
+                nodes+=1<<(h-1);
+                root=root->right;
+            }
+            else
+            {
+                nodes+=1<<(h-2);
+                root=root->left;
+            }
+            h--;
         }
-        return 0;
+        return nodes;
     }
 };
