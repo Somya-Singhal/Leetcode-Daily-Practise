ccf4cc8a8309bf49f10bbb2127df95dc268b9c23
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-28 01:08:38 +0530

    Update

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,26 +10,34 @@
 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* curr=root;
-        if(p->val>q->val)
-            {
-               TreeNode* temp=p;
-               p=q;
-               q=temp;
-            }
-        while(curr!=NULL)
-        {
-            if(curr->val>p->val && curr->val<q->val)
-              return curr;
-            if(curr->val==p->val || curr->val==q->val)
-                return curr;
-            if(curr->val<p->val)
-                curr=curr->right;
-            else
-                curr=curr->left;
-        }
-        return NULL;
+    //     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         TreeNode* curr=root;
+//         if(p->val>q->val)
+//             {
+//                TreeNode* temp=p;
+//                p=q;
+//                q=temp;
+//             }
+//         while(curr!=NULL)
+//         {
+//             if(curr->val>p->val && curr->val<q->val)
+//               return curr;
+//             if(curr->val==p->val || curr->val==q->val)
+//                 return curr;
+//             if(curr->val<p->val)
+//                 curr=curr->right;
+//             else
+//                 curr=curr->left;
+//         }
+//         return NULL;
         
+//     }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root->val>p->val && root->val>q->val)
+            return lowestCommonAncestor(root->left,p,q);
+        if(root->val<p->val && root->val<q->val)
+             return lowestCommonAncestor(root->right,p,q);
+        else
+            return root;
     }
 };
