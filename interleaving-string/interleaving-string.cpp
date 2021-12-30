17f3299ce6011c7402d4813923fb581dac953f61
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-30 13:17:14 +0530

    Update

--- interleaving-string/interleaving-string.cpp
+++ interleaving-string/interleaving-string.cpp
@@ -48,41 +48,77 @@ public:
     //     return Interleaverec(0,0,0,s1,s2,s3,n1,n2,n3);
     // }
     
-    //dp
-      bool isInterleave(string s1, string s2, string s3) {
-          int n1=s1.length();
-         int n2=s2.length();
-         int n3=s3.length();
-        if(n1+n2!=n3)
-            return false;
-          bool dp[n1+1][n2+1];
-          for(int i=0;i<=n1;i++)
-          {
-              for(int j=0;j<=n2;j++)
-              {
-                  if(i==0 && j==0)
-                      dp[i][j]=true;
-                  else if(i==0)
+    //dp-->2D array used-->space--->O(m*n)
+    //   bool isInterleave(string s1, string s2, string s3) {
+    //       int n1=s1.length();
+    //      int n2=s2.length();
+    //      int n3=s3.length();
+    //     if(n1+n2!=n3)
+    //         return false;
+    //       bool dp[n1+1][n2+1];
+    //       for(int i=0;i<=n1;i++)
+    //       {
+    //           for(int j=0;j<=n2;j++)
+    //           {
+    //               if(i==0 && j==0)
+    //                   dp[i][j]=true;
+    //               else if(i==0)
+    //               {
+    //                   if(s2[j-1]==s3[i+j-1])
+    //                       dp[i][j]=dp[i][j-1];
+    //                   else
+    //                       dp[i][j]=false;
+    //               }
+    //               else
+    //               {
+    //                   if(i!=0 && j!=0 && s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1])
+    //                       dp[i][j]=dp[i-1][j] || dp[i][j-1];
+    //                   else if(s1[i-1]==s3[i+j-1])
+    //                       dp[i][j]=dp[i-1][j];
+    //                   else if(j!=0 && s2[j-1]==s3[i+j-1])
+    //                       dp[i][j]=dp[i][j-1];
+    //                   else
+    //                       dp[i][j]=false;
+    //               }
+    //           }
+    //       }
+    //       return dp[n1][n2]; 
+    // }
+    
+    //dp-->space-->O(n)
+     bool isInterleave(string s1, string s2, string s3) {
+         int m=s1.length();
+         int n=s2.length();
+         if(m+n!=s3.length())
+             return false;
+         bool dp[n+1];
+         for(int i=0;i<=m;i++)
+         {
+             for(int j=0;j<=n;j++)
+             {
+                 if(i==0 && j==0)
+                     dp[j]=true;
+                 else if(i==0)
                   {
                       if(s2[j-1]==s3[i+j-1])
-                          dp[i][j]=dp[i][j-1];
+                          dp[j]=dp[j-1];
                       else
-                          dp[i][j]=false;
+                          dp[j]=false;
                   }
-                  else
+                else
                   {
                       if(i!=0 && j!=0 && s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1])
-                          dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                          dp[j]=dp[j] || dp[j-1];
                       else if(s1[i-1]==s3[i+j-1])
-                          dp[i][j]=dp[i-1][j];
+                         continue;
                       else if(j!=0 && s2[j-1]==s3[i+j-1])
-                          dp[i][j]=dp[i][j-1];
+                          dp[j]=dp[j-1];
                       else
-                          dp[i][j]=false;
+                          dp[j]=false;
                   }
-              }
-          }
-          return dp[n1][n2]; 
-    }
+             }
+         }
+         return dp[n];
+     }
     
 };
