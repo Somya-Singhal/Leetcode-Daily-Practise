afed7abc634225f8e49040d9439986abc274c35c
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-31 20:51:29 +0530

    Update

--- reconstruct-itinerary/reconstruct-itinerary.cpp
+++ reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,8 +1,49 @@
 class Solution {
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
+       //     vector<string> findItinerary(vector<vector<string>>& tickets) {
+//         unordered_map<string,multiset<string>>adj;
+//         stack<string>s;
+//         for(auto str:tickets)
+//         {
+//             string s1=str[0];
+//             string s2=str[1];
+//             adj[s1].insert({s2});
+//         }
+//         vector<string>res;
+//         s.push("JFK");
+//         while(!s.empty())
+//         {
+//             string front=s.top();
+//             if(adj[front].size()==0)
+//             {
+//                 res.push_back(front);
+//                 s.pop();
+//             }
+//             else
+//             {
+//                     auto it=adj[front].begin();
+//                     s.push(*it);
+//                     adj[front].erase(it);
+        
+//             }
+//         }
+//         reverse(res.begin(),res.end());
+//         return res;
+//     }
+     void dfs(string s,unordered_map<string,multiset<string>>& adj,vector<string>& res)
+     {
+         while(adj[s].size())
+         {
+                 auto it=adj[s].begin();
+                 string newst=*it;
+                 adj[s].erase(it);
+                 dfs(newst,adj,res);
+         }
+        res.push_back(s);
+     }
+     vector<string> findItinerary(vector<vector<string>>& tickets) {
         unordered_map<string,multiset<string>>adj;
-        stack<string>s;
         for(auto str:tickets)
         {
             string s1=str[0];
@@ -10,23 +51,7 @@ public:
             adj[s1].insert({s2});
         }
         vector<string>res;
-        s.push("JFK");
-        while(!s.empty())
-        {
-            string front=s.top();
-            if(adj[front].size()==0)
-            {
-                res.push_back(front);
-                s.pop();
-            }
-            else
-            {
-                    auto it=adj[front].begin();
-                    s.push(*it);
-                    adj[front].erase(it);
-        
-            }
-        }
+        dfs("JFK",adj,res);
         reverse(res.begin(),res.end());
         return res;
     }
