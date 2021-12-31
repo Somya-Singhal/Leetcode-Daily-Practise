76470e4a4bac5cca1d053252c78b7520d753bcba
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2021-12-31 15:19:52 +0530

    update

--- verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
+++ verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    // bool isValidSerialization(string preorder) {
+    //   stringstream ss(preorder);
+    //   int degree=1;
+    //     while(ss.good())
+    //     {
+    //         string substr;
+    //         getline(ss,substr,',');
+    //          if(--degree<0)
+    //           return false;
+    //          if(substr!="#")
+    //           degree+=2;
+    //     }
+    //     return degree==0;
+    // }
+    
+    //stack used
     bool isValidSerialization(string preorder) {
-      stringstream ss(preorder);
-      int degree=1;
+        stack<string>s;
+        stringstream ss(preorder);
         while(ss.good())
         {
-            string substr;
-            getline(ss,substr,',');
-             if(--degree<0)
-              return false;
-             if(substr!="#")
-              degree+=2;
+            string str;
+            getline(ss,str,',');
+            while(str=="#" && !s.empty() && s.top()=="#")
+            {
+                s.pop();
+                if(s.empty())
+                    return false;
+                s.pop();
+            }
+            s.push(str);
         }
-        return degree==0;
+        return s.size()==1 && s.top()=="#";
     }
 };
