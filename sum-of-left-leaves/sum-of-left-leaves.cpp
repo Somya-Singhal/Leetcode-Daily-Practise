3328edb830008cb7fc5170707fb046e469733667
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-02 12:15:25 +0530

    Update

--- sum-of-left-leaves/sum-of-left-leaves.cpp
+++ sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -12,21 +12,58 @@
 class Solution {
 public:
    
-    int sumOfLeftLeaves(TreeNode* root) {
-        int ans=0;
-       stack<TreeNode*>s;
-       s.push(root);
-        while(!s.empty())
-        {
-           TreeNode* curr=s.top();
-           s.pop();
-           if(curr->right!=NULL)
-               s.push(curr->right);
-           if(curr->left!=NULL && curr->left->left==NULL && curr->left->right==NULL)
-               ans+=curr->left->val;
-           else if(curr->left!=NULL)
-            s.push(curr->left);
-        }
-        return ans;
-    }
+   //using stack
+    // int sumOfLeftLeaves(TreeNode* root) {
+    //     int ans=0;
+    //    stack<TreeNode*>s;
+    //    s.push(root);
+    //     while(!s.empty())
+    //     {
+    //        TreeNode* curr=s.top();
+    //        s.pop();
+    //        if(curr->right!=NULL)
+    //            s.push(curr->right);
+    //        if(curr->left!=NULL && curr->left->left==NULL && curr->left->right==NULL)
+    //            ans+=curr->left->val;
+    //        else if(curr->left!=NULL)
+    //         s.push(curr->left);
+    //     }
+    //     return ans;
+    // }
+     
+    //  int sumOfLeftLeaves(TreeNode* root) {
+    //    if(root==NULL)
+    //        return 0;
+    //      int ans=0;
+    //      queue<TreeNode*>q;
+    //      q.push(root);
+    //      while(!q.empty())
+    //      {
+    //          int len=q.size();
+    //          for(int i=0;i<len;i++)
+    //          {
+    //              TreeNode* curr=q.front();
+    //              q.pop();
+    //              if(curr->left && curr->left->left==NULL && curr->left->right==NULL)
+    //                  ans+=curr->left->val;
+    //              else if(curr->left)
+    //                  q.push(curr->left);
+    //              if(curr->right)
+    //                  q.push(curr->right);
+    //          }
+    //      }
+    //      return ans;
+    // }
+     int sumOfLeftLeaves(TreeNode* root) {
+         if(root==NULL)
+             return 0;
+       if(root->left==NULL && root->right==NULL)
+          return 0;
+       int ans=0;
+       if(root->left!=NULL && root->left->left==NULL && root->left->right==NULL)
+           ans+=root->left->val;
+       ans+=sumOfLeftLeaves(root->left);
+       ans+=sumOfLeftLeaves(root->right);
+       return ans;
+     }
 };
