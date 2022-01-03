c2893298a65df2472ebb9a1e2c8adc2b5d020c1b
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-04 00:21:05 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (71.70%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
-    vector<int> diffWaysToCompute(string expression) {
+    //recursion
+    // vector<int> diffWaysToCompute(string expression) {
+    //     int n=expression.length();
+    //     vector<int>res;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         int curr=expression[i];
+    //         if(curr=='+' || curr=='-' || curr=='*')
+    //         {
+    //             vector<int>l=diffWaysToCompute(expression.substr(0,i));
+    //             vector<int>r=diffWaysToCompute(expression.substr(i+1));
+    //             for(auto x:l)
+    //             {
+    //                 for(auto y:r)
+    //                 {
+    //                     if(curr=='+')
+    //                         res.push_back(x+y);
+    //                     else if(curr=='-')
+    //                         res.push_back(x-y);
+    //                     else if(curr=='*')
+    //                         res.push_back(x*y);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     if(res.empty())
+    //     {
+    //         res.push_back(atoi(expression.c_str()));
+    //     }
+    //     return res;
+    // }
+    
+    //memoization
+    vector<int> diffWaysrec(string expression,unordered_map<string,vector<int>>& mp)
+    {
         int n=expression.length();
         vector<int>res;
         for(int i=0;i<n;i++)
@@ -8,8 +42,17 @@ public:
             int curr=expression[i];
             if(curr=='+' || curr=='-' || curr=='*')
             {
-                vector<int>l=diffWaysToCompute(expression.substr(0,i));
-                vector<int>r=diffWaysToCompute(expression.substr(i+1));
+                string str1=expression.substr(0,i);
+                vector<int>l,r;
+                if(mp.find(str1)!=mp.end())
+                    l=mp[str1];
+                else
+                   l=diffWaysrec(str1,mp);
+                string str2=expression.substr(i+1);
+                if(mp.find(str2)!=mp.end())
+                    r=mp[str2];
+                else
+                    r=diffWaysrec(str2,mp);
                 for(auto x:l)
                 {
                     for(auto y:r)
@@ -28,6 +71,10 @@ public:
         {
             res.push_back(atoi(expression.c_str()));
         }
-        return res;
+        return mp[expression]=res; 
+    }
+    vector<int> diffWaysToCompute(string expression) {
+        unordered_map<string,vector<int>>mp;
+        return diffWaysrec(expression,mp);
     }
 };
