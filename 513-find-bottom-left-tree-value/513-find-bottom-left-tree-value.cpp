a9b50ec1a78f57c8a0a658bc531f88471e660c15
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-04 10:49:49 +0530

    Time: 22 ms (10.64%), Space: 21.8 MB (57.90%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
@@ -11,25 +11,52 @@
  */
 class Solution {
 public:
-    void findBottom(TreeNode* root,int &maxdepth,int depth,int &ans)
-    {
-        if(root==NULL)
-            return;
-        if(depth>maxdepth)
-        {
-           ans=root->val;
-           maxdepth=depth;
-        }
-        findBottom(root->left,maxdepth,depth+1,ans);
-        findBottom(root->right,maxdepth,depth+1,ans);
-        return;
-    }
-    int findBottomLeftValue(TreeNode* root) {
-        if(root==NULL)
-            return 0;
-        int ans=0;
-        int maxdepth=-1;
-        findBottom(root,maxdepth,0,ans);
-        return ans;
-    }
+    //dfs-->preorder
+    //time->O(n),space-->height balanced(O(logn)),else-->O(n)
+    // void findBottom(TreeNode* root,int &maxdepth,int depth,int &ans)
+    // {
+    //     if(root==NULL)
+    //         return;
+    //     if(depth>maxdepth)
+    //     {
+    //        ans=root->val;
+    //        maxdepth=depth;
+    //     }
+    //     findBottom(root->left,maxdepth,depth+1,ans);
+    //     findBottom(root->right,maxdepth,depth+1,ans);
+    //     return;
+    // }
+    // int findBottomLeftValue(TreeNode* root) {
+    //     if(root==NULL)
+    //         return 0;
+    //     int ans=0;
+    //     int maxdepth=-1;
+    //     findBottom(root,maxdepth,0,ans);
+    //     return ans;
+    // }
+    
+    //bfs
+     int findBottomLeftValue(TreeNode* root) {
+         if(root==NULL)
+             return 0;
+         queue<TreeNode*>q;
+         q.push(root);
+         int ans=0;
+         while(!q.empty())
+         {
+             int len=q.size();
+             for(int i=0;i<len;i++)
+             {
+                 TreeNode* curr=q.front();
+                 q.pop();
+                 if(i==0)
+                 ans=curr->val;
+                 if(curr->left)
+                     q.push(curr->left);
+                 if(curr->right)
+                     q.push(curr->right);
+             }
+         }
+         return ans;
+     }
 };
