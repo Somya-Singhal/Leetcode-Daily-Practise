beb336715b76b15508b2f63d2340b55fcd0c9781
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-04 18:55:55 +0530

    Time: 1710 ms (5.04%), Space: 365.8 MB (5.04%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,41 +1,71 @@
 class Solution {
 public:
+    //time->O(V+E)
+    //space-->O(V)
+    // vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    //     vector<int>res;
+    //     int n=graph.size();
+    //     vector<bool>safe(n,false);
+    //     vector<vector<int>>rgraph(n,vector<int>());
+    //     queue<int>q;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(graph[i].size()==0)
+    //             q.push(i);
+    //         for(auto x:graph[i])
+    //             rgraph[x].push_back(i);
+    //     }
+    //     while(!q.empty())
+    //     {
+    //         int j=q.front();
+    //         safe[j]=true;
+    //         q.pop();
+    //         for(auto x:rgraph[j])
+    //         {
+    //             for(int i=0;i<graph[x].size();i++)
+    //             {
+    //                 if(graph[x][i]==j)
+    //                 {
+    //                     graph[x].erase(graph[x].begin()+i);
+    //                     break;
+    //                 }
+    //             }
+    //             if(graph[x].empty())
+    //                 q.push(x);
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(safe[i])
+    //             res.push_back(i);
+    //     }
+    //     return res;
+    // }
+    
+    //dfs
+    bool dfs(int src,vector<vector<int>>& graph,vector<bool>& rect,vector<bool>& safe)
+    {
+        safe[src]=true;
+        rect[src]=true;
+        for(auto x:graph[src])
+        {
+              if(!safe[x] && dfs(x,graph,rect,safe))
+                    return true;
+              else if(rect[x]==true)
+                    return true;
+        }
+        rect[src]=false;
+        return false;
+    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         vector<int>res;
         int n=graph.size();
-        vector<bool>safe(n,false);
-        vector<vector<int>>rgraph(n,vector<int>());
-        queue<int>q;
         for(int i=0;i<n;i++)
         {
-            if(graph[i].size()==0)
-                q.push(i);
-            for(auto x:graph[i])
-                rgraph[x].push_back(i);
-        }
-        while(!q.empty())
-        {
-            int j=q.front();
-            safe[j]=true;
-            q.pop();
-            for(auto x:rgraph[j])
-            {
-                for(int i=0;i<graph[x].size();i++)
-                {
-                    if(graph[x][i]==j)
-                    {
-                        graph[x].erase(graph[x].begin()+i);
-                        break;
-                    }
-                }
-                if(graph[x].empty())
-                    q.push(x);
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(safe[i])
-                res.push_back(i);
+            vector<bool>safe(n,false);
+            vector<bool>rect(n,false);
+            if(dfs(i,graph,rect,safe)==false)
+                res.push_back(i);       
         }
         return res;
     }
