a9bf18bd7027453d3c9a04d610545b977488e74f
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-04 10:25:42 +0530

    Time: 29 ms (5.24%), Space: 22.1 MB (66.50%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -11,29 +11,54 @@
  */
 class Solution {
 public:
+    
+    //level order traversal
+//     vector<int> largestValues(TreeNode* root) {
+//         if(root==NULL)
+//             return {};
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         vector<int>res;
+//         while(!q.empty())
+//         {
+//             int len=q.size();
+//             int rowmax=INT_MIN;
+//             for(int i=0;i<len;i++)
+//             {
+//                 TreeNode* curr=q.front();
+//                 q.pop();
+//                 rowmax=max(rowmax,curr->val);
+//                 if(curr->left)
+//                     q.push(curr->left);
+//                 if(curr->right)
+//                     q.push(curr->right);
+                
+//             }
+//             res.push_back(rowmax);
+//         }
+//         return res;
+//     }
+    
+    //pre-order traversal
+    void largestvalpre(TreeNode* root,int depth,vector<int>& res)
+    {
+        if(root==NULL)
+            return;
+        if(depth==res.size())
+        {
+                res.push_back(root->val);
+        }
+        else
+            res[depth]=max(res[depth],root->val);
+        largestvalpre(root->left,depth+1,res);
+        largestvalpre(root->right,depth+1,res);
+        return;
+    }
     vector<int> largestValues(TreeNode* root) {
         if(root==NULL)
             return {};
-        queue<TreeNode*>q;
-        q.push(root);
         vector<int>res;
-        while(!q.empty())
-        {
-            int len=q.size();
-            int rowmax=INT_MIN;
-            for(int i=0;i<len;i++)
-            {
-                TreeNode* curr=q.front();
-                q.pop();
-                rowmax=max(rowmax,curr->val);
-                if(curr->left)
-                    q.push(curr->left);
-                if(curr->right)
-                    q.push(curr->right);
-                
-            }
-            res.push_back(rowmax);
-        }
+        largestvalpre(root,0,res);
         return res;
     }
 };
