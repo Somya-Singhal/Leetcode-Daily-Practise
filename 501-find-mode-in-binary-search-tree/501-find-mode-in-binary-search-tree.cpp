de35c283e894379d0db1b331e247f11218c33a03
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-07 18:12:48 +0530

    Time: 18 ms (67.50%), Space: 23.8 MB (97.06%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
@@ -11,31 +11,66 @@
  */
 class Solution {
 public:
-    void countofElements(TreeNode* root,unordered_map<int,int>& mp)
+    // void countofElements(TreeNode* root,unordered_map<int,int>& mp)
+    // {
+    //     if(root==NULL)
+    //         return;
+    //     mp[root->val]++;
+    //     countofElements(root->left,mp);
+    //     countofElements(root->right,mp);
+    //     return;
+    // }
+    // vector<int> findMode(TreeNode* root) {
+    //     vector<int>res;
+    //     unordered_map<int,int>mp;
+    //     countofElements(root,mp);
+    //     int countmax=INT_MIN;
+    //     for(auto x: mp)
+    //     {
+    //        if(countmax<x.second)
+    //        {
+    //            res.clear();
+    //            res.push_back(x.first);
+    //            countmax=x.second;
+    //        }
+    //         else if(countmax==x.second)
+    //             res.push_back(x.first);
+    //     }
+    //     return res;
+    // }
+    
+    //without extra space(except recursion)-->inorder traversal
+    int maxcount=0,currcount=0,currval;
+     vector<int>res;
+    void inorder(TreeNode* root)
     {
         if(root==NULL)
             return;
-        mp[root->val]++;
-        countofElements(root->left,mp);
-        countofElements(root->right,mp);
-        return;
+        inorder(root->left);
+        countofElements(root->val);
+        inorder(root->right);
+    }
+     void countofElements(int value)
+    {
+        if(value!=currval)
+        {
+            currval=value;
+            currcount=0;
+        }
+        currcount++;
+        if(currcount>maxcount)
+        {
+            res.clear();
+            res.push_back(currval);
+            maxcount=currcount;
+        }
+        else if(currcount==maxcount)
+        {
+            res.push_back(currval);
+        }
     }
     vector<int> findMode(TreeNode* root) {
-        vector<int>res;
-        unordered_map<int,int>mp;
-        countofElements(root,mp);
-        int countmax=INT_MIN;
-        for(auto x: mp)
-        {
-           if(countmax<x.second)
-           {
-               res.clear();
-               res.push_back(x.first);
-               countmax=x.second;
-           }
-            else if(countmax==x.second)
-                res.push_back(x.first);
-        }
+        inorder(root);
         return res;
     }
 };
