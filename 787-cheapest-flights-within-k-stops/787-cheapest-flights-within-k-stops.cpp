012730015762291f67eadfda24d12e7a69c6f78b
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-14 12:40:34 +0530

    Time: 47 ms (38.73%), Space: 13.8 MB (43.62%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -39,6 +39,26 @@ public:
     // }
     
     //Bellmann Ford as k stops are also mentioned so it will be more easier
+    //time->
+    //  int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    //     vector<int>dist(n,INT_MAX); 
+    //      dist[src]=0;
+    //      vector<int>temp=dist;
+    //     for(int i=0;i<k+1;i++)
+    //     {
+    //         for(auto x:flights)
+    //         {
+    //             if(dist[x[0]]==INT_MAX)
+    //                 continue;
+    //             if(temp[x[1]]>dist[x[0]]+x[2])
+    //                 temp[x[1]]=dist[x[0]]+x[2];
+    //         }
+    //         dist=temp;
+    //     }
+    //      return dist[dst]==INT_MAX?-1:dist[dst];
+    // }
+    
+    //dijkstra algo
      int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>>adj(n,vector<pair<int,int>>());
         vector<int>dist(n,INT_MAX); 
@@ -50,18 +70,57 @@ public:
             adj[u].push_back(make_pair(v,wt));
         }
          dist[src]=0;
-         vector<int>temp=dist;
-        for(int i=0;i<k+1;i++)
+         k++;
+         list<pair<int,pair<int,int>>>li;
+         li.push_back(make_pair(src,make_pair(0,0)));
+         int mymin=INT_MAX;
+        while(!li.empty())
         {
-            for(auto x:flights)
+            int u=li.front().first,step=li.front().second.first,wt=li.front().second.second;
+            li.pop_front();
+            if(u==dst)
+                mymin=min(mymin,wt);
+            if(step==k)
+                continue;
+            step+=1;
+            for(auto x:adj[u])
             {
-                if(dist[x[0]]==INT_MAX)
-                    continue;
-                if(temp[x[1]]>dist[x[0]]+x[2])
-                    temp[x[1]]=dist[x[0]]+x[2];
+                int v=x.first,wtu=x.second;
+                if(dist[v]>wt+wtu)
+                {
+                      dist[v]=wt+wtu;
+                      li.push_back(make_pair(v,make_pair(step,dist[v])));
+                }
             }
-            dist=temp;
         }
-         return dist[dst]==INT_MAX?-1:dist[dst];
+         return mymin==INT_MAX?-1:mymin;
+//      unordered_map<int, vector<pair<int, int>>> mp;
+//         for (int i = 0; i < flights.size(); i++) {
+//             mp[flights[i][0]].push_back(make_pair(flights[i][1], flights[i][2]));
+//         }
+//         k += 1;
+//         list<pair<int, pair<int, int>>> ver;
+//         ver.push_back(make_pair(src, make_pair(0, 0)));
+//         vector<int> dp(n, INT_MAX);
+//         dp[src] = 0;
+//         int mini = INT_MAX;
+        
+//         while (!ver.empty()) {
+//             int u = ver.front().first, steps = ver.front().second.first, w = ver.front().second.second;
+//             //cout << u << ":" << w << ":" << steps << endl;
+//             ver.pop_front();
+//             if (u == dst)   mini = min(mini, w);
+//             if (steps == k) continue;
+//             steps += 1;
+//             for (int i = 0; i < mp[u].size(); i++) {
+//                 int nd = mp[u][i].first, dis = mp[u][i].second;
+//                 if (dp[nd] > w + dis) {
+//                     dp[nd] =  w + dis;
+//                     ver.push_back(make_pair(nd, make_pair(steps, w + dis)));
+//                 } 
+//             }
+//         }
+        
+//         return (mini == INT_MAX) ? -1 : mini;
     }
 };
