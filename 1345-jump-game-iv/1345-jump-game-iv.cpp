3b6783bf2b1e2524d3b8b486ceeae3310bb4a101
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-17 20:18:07 +0530

    Time: 164 ms (93.84%), Space: 67.8 MB (83.39%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -4,6 +4,7 @@ public:
     int minJumps(vector<int>& arr) {
         unordered_map<int,vector<int>>mp;
         int n=arr.size();
+         vector<bool>vis(n,false);
         if(n==1)
             return 0;
         int steps=0;
@@ -20,24 +21,30 @@ public:
             for(int i=0;i<len;i++)
             {
                 int j=q.front();
+                vis[j]=true;
                 q.pop();
-                if(j>0 && mp.find(arr[j-1])!=mp.end())
-                    q.push(j-1);
-                if(j<n-1 && mp.find(arr[j+1])!=mp.end())
+                if(j>0 && mp.find(arr[j-1])!=mp.end() && !vis[j-1])
+                {
+                   q.push(j-1);
+                    vis[j-1]=true;
+                }
+                if(j<n-1 && mp.find(arr[j+1])!=mp.end() && !vis[j+1])
                 {
                     if(j+1==n-1)
                         return steps;
                     q.push(j+1);
+                    vis[j+1]=true;
                 }
                 if(mp.find(arr[j])!=mp.end())
                 {
                     for(auto x:mp[arr[j]])
                     {
-                        if(x!=j)
+                        if(x!=j  && !vis[x])
                         {
                             if(x==n-1)
                                 return steps;
                             q.push(x);
+                            vis[x]=true;
                         }
                     }
                 }
