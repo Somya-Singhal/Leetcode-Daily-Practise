bd70274c523060b6458d53d8216426c206900bdf
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-24 21:18:11 +0530

    Time: 104 ms (96.15%), Space: 8.3 MB (69.23%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -1,37 +1,63 @@
 class Solution {
 public:
-    int n,ans=0;
-    bool isvalid(vector<vector<int>>& statements,string& curr)
+    // int n,ans=0;
+    // bool isvalid(vector<vector<int>>& statements,string& curr)
+    // {
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(curr[i]=='1')
+    //             for(int j=0;j<n;j++)
+    //             {
+    //                 if(statements[i][j]!=2 && statements[i][j]!=curr[j]-'0')
+    //                     return false;
+    //             }
+    //     }
+    //     return true;
+    // }
+    // int maximumGood(vector<vector<int>>& statements) {
+    //     n=statements.size();
+    //     string curr="";
+    //     dfs(statements,curr,0,0);
+    //         return ans;
+    // }
+    // void dfs(vector<vector<int>>& statements,string& curr,int i,int cnt)
+    // {
+    //     if(i==n)
+    //     {
+    //         if(isvalid(statements,curr))
+    //             ans=max(ans,cnt);
+    //         return;
+    //     }
+    //     curr.push_back('0');
+    //     dfs(statements,curr,i+1,cnt);
+    //     curr.back()='1';
+    //     dfs(statements,curr,i+1,cnt+1);
+    //     curr.pop_back();
+    // }
+    int n;
+    bool isvalid(vector<vector<int>>& statements,int num)
     {
         for(int i=0;i<n;i++)
         {
-            if(curr[i]=='1')
+            if(num & (1<<i))
+            {
                 for(int j=0;j<n;j++)
                 {
-                    if(statements[i][j]!=2 && statements[i][j]!=curr[j]-'0')
+                    if(statements[i][j]!=2 && statements[i][j]!=bool(num & (1<<j)))
                         return false;
                 }
+            }
         }
         return true;
     }
     int maximumGood(vector<vector<int>>& statements) {
         n=statements.size();
-        string curr="";
-        dfs(statements,curr,0,0);
-            return ans;
-    }
-    void dfs(vector<vector<int>>& statements,string& curr,int i,int cnt)
-    {
-        if(i==n)
+        int ans=0;
+        for(int i=1;i<(1<<n);i++)
         {
-            if(isvalid(statements,curr))
-                ans=max(ans,cnt);
-            return;
+            if(isvalid(statements,i))
+                ans=max(ans,__builtin_popcount(i));
         }
-        curr.push_back('0');
-        dfs(statements,curr,i+1,cnt);
-        curr.back()='1';
-        dfs(statements,curr,i+1,cnt+1);
-        curr.pop_back();
+        return ans;
     }
 };
