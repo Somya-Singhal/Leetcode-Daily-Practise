a87d6cb1d7813b97cf6a0ee83bb7d638a0a7e8bd
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-25 02:38:39 +0530

    Time: 28 ms (20.30%), Space: 10.7 MB (69.41%) - LeetHub

--- 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.cpp
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.cpp
@@ -20,26 +20,51 @@ public:
 
 class Solution {
 public:
+    //time:O(n), space:O(width);
+    //BFS
+    // int maxDepth(Node* root) {
+    //     if(root==NULL)
+    //         return 0;
+    //    queue<Node*>q;
+    //    int depth=0;
+    //    q.push(root);
+    //     while(!q.empty())
+    //     {
+    //         int len=q.size();
+    //         for(int i=0;i<len;i++)
+    //         {
+    //             Node* curr=q.front();
+    //             q.pop();
+    //             for(int j=0;j<curr->children.size();j++)
+    //             {
+    //                 q.push(curr->children[j]);
+    //             }
+    //         }
+    //         depth++;
+    //     }
+    //     return depth;
+    // }
+    
+    //DFS
+    int maxdepth=0;
+    void dfs(Node* root,int depth)//passed depth by reference(C++)
+    {
+        if(root==NULL)
+            return;
+        if(maxdepth<depth)
+        {
+            maxdepth=depth;
+        }
+        for(int i=0;i<root->children.size();i++)
+        {
+            dfs(root->children[i],depth+1);
+        }
+        return;
+    }
     int maxDepth(Node* root) {
         if(root==NULL)
             return 0;
-       queue<Node*>q;
-       int depth=0;
-       q.push(root);
-        while(!q.empty())
-        {
-            int len=q.size();
-            for(int i=0;i<len;i++)
-            {
-                Node* curr=q.front();
-                q.pop();
-                for(int j=0;j<curr->children.size();j++)
-                {
-                    q.push(curr->children[j]);
-                }
-            }
-            depth++;
-        }
-        return depth;
+       dfs(root,1);
+        return maxdepth;
     }
 };
