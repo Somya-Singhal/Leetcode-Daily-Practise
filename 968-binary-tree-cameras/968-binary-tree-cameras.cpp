1f17c8f3eb56d7e212167f8885ff10f9ac494165
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-01-26 23:49:40 +0530

    Time: 25 ms (15.43%), Space: 23.8 MB (14.26%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -11,25 +11,50 @@
  */
 class Solution {
 public:
-    int camera=0;
-    int solve(TreeNode* root)
+    // int camera=0;
+    // int solve(TreeNode* root)
+    // {
+    //     if(root==NULL)
+    //         return 1;
+    //     int lval=solve(root->left);
+    //     int rval=solve(root->right);
+    //     if(lval==-1 || rval==-1)
+    //     {
+    //         camera++;
+    //         return 0;
+    //     }
+    //     if(lval==0 || rval==0)
+    //         return 1;
+    //     return -1;
+    // }
+    // int minCameraCover(TreeNode* root) {
+    //     if(solve(root)==-1)
+    //         camera++;
+    //     return camera;
+    // }
+    
+    //greedy->intuitive sol :)//
+    int ans=0;
+    unordered_set<TreeNode*>covered;
+    void solve(TreeNode* root,TreeNode* par)
     {
-        if(root==NULL)
-            return 1;
-        int lval=solve(root->left);
-        int rval=solve(root->right);
-        if(lval==-1 || rval==-1)
+        if(root!=NULL)
         {
-            camera++;
-            return 0;
+            solve(root->left,root);
+            solve(root->right,root);
+        if((par==NULL && covered.find(root)==covered.end()) || (covered.find(root->left)==covered.end() || covered.find(root->right)==covered.end()))
+        {
+            ans++;
+            covered.insert({root});
+            covered.insert({par});
+            covered.insert({root->left});
+            covered.insert({root->right});
         }
-        if(lval==0 || rval==0)
-            return 1;
-        return -1;
+        }
     }
     int minCameraCover(TreeNode* root) {
-        if(solve(root)==-1)
-            camera++;
-        return camera;
-    }
+        covered.insert({NULL});
+        solve(root,NULL);
+        return ans;
+      }
 };
