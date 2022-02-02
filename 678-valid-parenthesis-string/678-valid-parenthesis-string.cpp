46b6fc9ac20b301666c051154b5efe2a919244f9
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-02 14:28:59 +0530

    Time: 2 ms (32.84%), Space: 6.2 MB (53.76%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -16,17 +16,49 @@ public:
     // }
     
     //greedy->O(n) (but not intuitive :') 
-     bool checkValidString(string s) {
+    //1st approach
+    //  bool checkValidString(string s) {
+    //      int n=s.length();
+    //      int lo=0,hi=0;
+    //      for(int i=0;i<n;i++)
+    //      {
+    //          lo+=s[i]=='('?1:-1;
+    //          hi+=s[i]!=')'?1:-1;
+    //          if(hi<0)
+    //              break;
+    //          lo=max(lo,0);
+    //      }
+    //      return lo==0;
+    // }
+    
+    //2nd approach
+    bool checkValidString(string s) {
          int n=s.length();
-         int lo=0,hi=0;
+         int leftmin=0,leftmax=0;
          for(int i=0;i<n;i++)
          {
-             lo+=s[i]=='('?1:-1;
-             hi+=s[i]!=')'?1:-1;
-             if(hi<0)
-                 break;
-             lo=max(lo,0);
+             if(s[i]=='(')
+             {
+                 leftmin++;
+                 leftmax++;
+             }
+             else if(s[i]==')')
+             {
+                leftmin--;
+                 leftmax--; 
+             }
+                 
+             else
+             {
+                 leftmin--;
+                 leftmax++;
+             }
+             if(leftmax<0)
+                 return false;
+             if(leftmin<0)
+                 leftmin=0;
+             
          }
-         return lo==0;
+         return leftmin==0;
     }
 };
