e2703957de905db9be226cf0e37c5f07e1d8655a
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-03 15:15:34 +0530

    Time: 427 ms (23.77%), Space: 24.2 MB (94.70%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        unordered_map<int,int>m1,m2;
+    // int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+    //     unordered_map<int,int>m1,m2;
+    //     int n1=nums1.size(),n2=nums2.size(),n3=nums3.size(),n4=nums4.size();
+    //     int count=0;
+    //     for(int i=0;i<n1;i++)
+    //     {
+    //         for(int j=0;j<n2;j++)
+    //         {
+    //             int sum=nums1[i]+nums2[j];
+    //             m1[sum]++;
+    //         }
+    //     }
+    //     for(int i=0;i<n3;i++)
+    //     {
+    //         for(int j=0;j<n4;j++)
+    //         {
+    //             int sum=nums3[i]+nums4[j];
+    //             m2[sum]++;
+    //         }
+    //     }
+    //     for(auto x:m1)
+    //     {
+    //         if(m2.find(-x.first)!=m2.end())
+    //             count+=x.second*m2[-x.first];
+    //     }
+    //     return count;
+    // }
+    
+     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int,int>m;
         int n1=nums1.size(),n2=nums2.size(),n3=nums3.size(),n4=nums4.size();
         int count=0;
-        for(int i=0;i<n1;i++)
-        {
-            for(int j=0;j<n2;j++)
-            {
-                int sum=nums1[i]+nums2[j];
-                m1[sum]++;
-            }
-        }
         for(int i=0;i<n3;i++)
         {
             for(int j=0;j<n4;j++)
             {
                 int sum=nums3[i]+nums4[j];
-                m2[sum]++;
+                m[sum]++;
             }
         }
-        for(auto x:m1)
+        for(int i=0;i<n1;i++)
         {
-            if(m2.find(-x.first)!=m2.end())
-                count+=x.second*m2[-x.first];
+            for(int j=0;j<n2;j++)
+            {
+                if(m.find(-(nums1[i]+nums2[j]))!=m.end())
+                    count+=m[-(nums1[i]+nums2[j])]; 
+            }
         }
         return count;
     }
