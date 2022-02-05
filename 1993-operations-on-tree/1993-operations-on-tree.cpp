b93c6cf2be605d077facebc020abe5077a5fea3b
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-06 04:21:35 +0530

    Time: 1574 ms (5.23%), Space: 134.3 MB (51.57%) - LeetHub

---
+++ 1993-operations-on-tree/1993-operations-on-tree.cpp
@@ -0,0 +1,81 @@
+class LockingTree {
+public:
+    vector<vector<int>>nodes;
+    unordered_map<int,vector<int>>descendent;
+    int n;
+    LockingTree(vector<int>& parent) {
+        n=parent.size();
+        nodes.resize(n,vector<int>(2,-1));
+        nodes[0][0]=-1;
+        for(int i=1;i<n;i++)
+        {
+            nodes[i][0]=parent[i];
+            descendent[parent[i]].push_back(i);
+        }
+    }
+    
+    bool lock(int num, int user) {
+        if(nodes[num][1]!=-1)
+            return false;
+        nodes[num][1]=user;
+        return true;
+    }
+    
+    bool unlock(int num, int user) {
+        if(nodes[num][1]!=user)
+        return false;
+        nodes[num][1]=-1;
+        return true;
+    }
+   void countlockeddesc(int num,bool &present)
+    {
+        if(descendent.count(num)==0 || descendent[num].size()==0)
+        return;
+        for(auto &x: descendent[num])
+        {
+            if(nodes[x][1]!=-1)
+            {
+                present=true;
+                return;
+            }
+            countlockeddesc(x,present);
+        }
+    }
+    void removedescendent(int num)
+    {
+        if(descendent.count(num)==0  || descendent[num].size()==0)
+            return;
+        for(auto x: descendent[num])
+        {
+            nodes[x][1]=-1;
+           removedescendent(x); 
+        }
+    }
+    bool ancestor(int num)
+    {
+        if(num==-1)
+            return false;
+        return nodes[num][1]!=-1 || ancestor(nodes[num][0]);
+    }
+    bool upgrade(int num, int user) {
+        if(nodes[num][1]!=-1)
+            return false;
+         bool present=false;
+        countlockeddesc(num,present);
+        if(!present)
+            return false;
+       if(ancestor(nodes[num][0]))
+           return false;
+        removedescendent(num);  
+        nodes[num][1]=user;
+        return true;
+    }
+};
+
+/**
+ * Your LockingTree object will be instantiated and called as such:
+ * LockingTree* obj = new LockingTree(parent);
+ * bool param_1 = obj->lock(num,user);
+ * bool param_2 = obj->unlock(num,user);
+ * bool param_3 = obj->upgrade(num,user);
+ */
