7b42534e0e9d469762eab24ef4cd8dbd0f1ac3a9
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-08 12:39:00 +0530

    Time: 404 ms (77.89%), Space: 17.2 MB (31.03%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,27 +1,58 @@
 class Solution {
 public:
     
-    int solve(vector<set<int>>& v,int target,int i,int sum,vector<vector<int>>& dp)
-    {
-        if(i>=v.size())
-            return abs(sum-target);
-        if(dp[i][sum]!=INT_MAX)
-            return dp[i][sum];
-        for(auto it=begin(v[i]);it!=end(v[i]);it++)
-        {
-            dp[i][sum]=min(dp[i][sum],solve(v,target,i+1,sum+*it,dp));
-            if(dp[i][sum]==0 || sum+*it>target)
-                break;
-        }
-        return dp[i][sum];
-    }
+    // int solve(vector<set<int>>& v,int target,int i,int sum,vector<vector<int>>& dp)
+    // {
+    //     if(i>=v.size())
+    //         return abs(sum-target);
+    //     if(dp[i][sum]!=INT_MAX)
+    //         return dp[i][sum];
+    //     for(auto it=begin(v[i]);it!=end(v[i]);it++)
+    //     {
+    //         dp[i][sum]=min(dp[i][sum],solve(v,target,i+1,sum+*it,dp));
+    //         if(dp[i][sum]==0 || sum+*it>target)
+    //             break;
+    //     }
+    //     return dp[i][sum];
+    // }
+    // int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+    //     vector<set<int>>v;
+    //     vector<vector<int>>dp(71,vector<int>(4901,INT_MAX));
+    //     for(auto &x: mat)
+    //     {
+    //         v.push_back(set<int>(begin(x),end(x)));
+    //     }
+    //     return solve(v,target,0,0,dp);
+    // }
+    
+    //using bitset
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
-        vector<set<int>>v;
-        vector<vector<int>>dp(71,vector<int>(4901,INT_MAX));
+        bool bt[70*70+1]={};
+        int max_e=0;
         for(auto &x: mat)
         {
-            v.push_back(set<int>(begin(x),end(x)));
+            int max_e1=0;
+            bool bt1[70*70+1]={};
+            for(auto i: unordered_set(begin(x),end(x)))
+            {
+                for(int j=0;j<=max_e;j++)
+                {
+                    if(j==max_e || bt[j])
+                    {
+                        bt1[i+j]=true;
+                        max_e1=max(max_e1,i+j);
+                    }
+                }
+            }
+            swap(bt,bt1);
+            max_e=max_e1;
         }
-        return solve(v,target,0,0,dp);
+        int res=INT_MAX;
+        for(int i=0;i<=70*70;i++)
+        {
+            if(bt[i])
+                res=min(res,abs(i-target));
+        }
+        return res;
     }
 };
