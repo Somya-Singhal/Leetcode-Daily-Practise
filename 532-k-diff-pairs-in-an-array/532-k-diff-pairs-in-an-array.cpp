2227eeb8aa1dfe0405763de19f3c180afd16aaa6
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-09 13:06:08 +0530

    Time: 47 ms (20.17%), Space: 13.8 MB (29.46%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,41 +1,63 @@
 class Solution {
 public:
-    int findPairs(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        set<pair<int,int>>s;
-        int n=nums.size();
-        int ans=0;
-        for(int i=0;i<n;i++)
-            mp[nums[i]]++;
-        for(int i=0;i<n;i++)
-        {
-                mp[nums[i]]--;
-                if(mp[nums[i]]==0)
-                    mp.erase(nums[i]);
-            int num1=nums[i]+k;
-            int num2=nums[i]-k;
-            if(s.count(make_pair(num1,nums[i]))==0 && s.count(make_pair(nums[i],num1))==0)
-            {
-                if(mp.find(num1)!=mp.end())
-                {
-                     ans++;
-                    s.insert(make_pair(nums[i],num1));
-                    s.insert(make_pair(num1,nums[i]));
-                }
-            }
-            if(s.count(make_pair(nums[i],num2))==0 && s.count(make_pair(num2,nums[i]))==0)
-            {
+//     int findPairs(vector<int>& nums, int k) {
+//         unordered_map<int,int>mp;
+//         set<pair<int,int>>s;
+//         int n=nums.size();
+//         int ans=0;
+//         for(int i=0;i<n;i++)
+//             mp[nums[i]]++;
+//         for(int i=0;i<n;i++)
+//         {
+//                 mp[nums[i]]--;
+//                 if(mp[nums[i]]==0)
+//                     mp.erase(nums[i]);
+//             int num1=nums[i]+k;
+//             int num2=nums[i]-k;
+//             if(s.count(make_pair(num1,nums[i]))==0 && s.count(make_pair(nums[i],num1))==0)
+//             {
+//                 if(mp.find(num1)!=mp.end())
+//                 {
+//                      ans++;
+//                     s.insert(make_pair(nums[i],num1));
+//                     s.insert(make_pair(num1,nums[i]));
+//                 }
+//             }
+//             if(s.count(make_pair(nums[i],num2))==0 && s.count(make_pair(num2,nums[i]))==0)
+//             {
                 
-                if(k!=0 && mp.find(num2)!=mp.end())
-                {
-                    ans++;
-                    s.insert(make_pair(nums[i],num2));
-                    s.insert(make_pair(num2,nums[i]));
-                }
+//                 if(k!=0 && mp.find(num2)!=mp.end())
+//                 {
+//                     ans++;
+//                     s.insert(make_pair(nums[i],num2));
+//                     s.insert(make_pair(num2,nums[i]));
+//                 }
                 
-            }
+//             }
             
-        }
-        return ans;
+//         }
+//         return ans;
+//     }
+    
+     int findPairs(vector<int>& nums, int k) {
+         int count=0;
+         map<int,int>mp;
+         int n=nums.size();
+         for(int i=0;i<n;i++)
+             mp[nums[i]]++;
+         for(auto &x: mp)
+         {
+             if(k==0)
+             {
+                 if(x.second>=2)
+                     count++;
+             }
+             else
+             {
+                 if(mp.find(x.first+k)!=mp.end())
+                     count++;
+             }
+         }
+         return count;
     }
 };
