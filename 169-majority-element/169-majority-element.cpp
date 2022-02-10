a00de7390d258595d4d256a0b74ed4984b18ea02
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-10 21:08:07 +0530

    Time: 26 ms (49.12%), Space: 19.7 MB (52.06%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int mooreVoting(vector<int>& nums)
+    {
+        int res=0,count=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]==nums[res])
+                count++;
+            else
+            count--;
+            if(count==0)
+            {
+                count=1;
+                res=i;
+            }
+        }
+        return res;
+    }
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int i=mooreVoting(nums);
+        int count=0;
+        // for(int j=0;j<n;j++)
+        // {
+        //     if(nums[j]==nums[i])
+        //         count++;
+        // }
+        return nums[i];
+    }
+};
