5cefc3aecadc78cb2e844693d06209914ba9f2c2
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-11 22:42:47 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (35.16%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // void solve(int idx,string s,string digits,vector<string>& res,vector<string>& mp)
+    // void solve(int idx,string s,string &digits,vector<string>& res,vector<string>& mp)
     // {
     //     if(idx==digits.size())
     //     {
@@ -12,28 +12,63 @@ public:
     //     return;
     // }
     
-     void solve(int idx,string s,string digits,vector<string>& res,vector<string>& mp)
-    {
-        if(idx==digits.size())
-        {
-            res.push_back(s);
-            return;
-        }
-        for(auto x:mp[digits[idx]-'2'])
-        {
-             s+=x;
-             solve(idx+1,s,digits,res,mp);
-             s.pop_back();
-        }
-        return;
-    }
+    //  void solve(int idx,string &s,string &digits,vector<string>& res,vector<string>& mp)
+    // {
+    //     if(idx==digits.size())
+    //     {
+    //         res.push_back(s);
+    //         return;
+    //     }
+    //     for(auto x:mp[digits[idx]-'2'])
+    //     {
+    //          s+=x;
+    //          solve(idx+1,s,digits,res,mp);
+    //          s.pop_back();
+    //     }
+    //     return;
+    // }
+    // vector<string> letterCombinations(string digits) {
+    //    int n=digits.length();
+    //     vector<string>mp={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    //     vector<string>res;
+    //     if(digits=="")
+    //         return res;
+    //     string str="";
+    //    solve(0,str,digits,res,mp);
+    //   return res;  
+    // }
+    
     vector<string> letterCombinations(string digits) {
        int n=digits.length();
         vector<string>mp={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         vector<string>res;
         if(digits=="")
             return res;
-       solve(0,"",digits,res,mp);
+        queue<string>q;
+        for(auto c:mp[digits[0]-'2'])
+        {
+            string s="";
+            s+=c;
+            q.push(s);
+        }
+        for(int i=1;i<n;i++)
+        {
+            int len=q.size();
+             while(len>0)
+            {
+                string s=q.front();
+                q.pop();
+                 len--;
+                for(auto c:mp[digits[i]-'2'])
+                q.push(s+c);
+            }
+        }
+        while(!q.empty())
+        {
+            string s=q.front();
+            res.push_back(s);
+            q.pop();
+        }
       return res;  
     }
 };
