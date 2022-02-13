e1e291dae9b9041c58bc81a95b4d2e980d8ea244
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-13 18:39:59 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.13%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -19,26 +19,44 @@ public:
     //     maxprod(1,n,0,1);
     //     return ans;
     // }
-    int num;
- int solve(int n,vector<int>& dp)
- {
-     if(dp[n]!=-1)
-         return dp[n];
-     if(n==num)
-         dp[n]=0;
-     else
-         dp[n]=n;
-     for(int i=1;i<n;i++)
-     {
-         int val=solve(i,dp)*solve(n-i,dp);
-         dp[n]=max(dp[n],val);
-     }
-     return dp[n];
- }
- int integerBreak(int n) {
+ //    int num;
+ // int solve(int n,vector<int>& dp)
+ // {
+ //     if(dp[n]!=-1)
+ //         return dp[n];
+ //     if(n==num)
+ //         dp[n]=0;
+ //     else
+ //         dp[n]=n;
+ //     for(int i=1;i<n;i++)
+ //     {
+ //         int val=solve(i,dp)*solve(n-i,dp);
+ //         dp[n]=max(dp[n],val);
+ //     }
+ //     return dp[n];
+ // }
+ // int integerBreak(int n) {
+ //     vector<int>dp(n+1,-1);
+ //     dp[1]=1;
+ //     num=n;
+ //    return solve(n,dp);
+ //    }
+    
+     int integerBreak(int n) {
      vector<int>dp(n+1,-1);
      dp[1]=1;
-     num=n;
-    return solve(n,dp);
+     for(int i=2;i<=n;i++)
+        {
+            if(i==n)
+              dp[i]=0;
+            else
+               dp[i]=i;
+            for(int j=1;j<i;j++)
+            {
+               int val=dp[j]*dp[i-j];
+               dp[i]=max(dp[i],val);
+            }
+        }
+        return dp[n];
     }
 };
