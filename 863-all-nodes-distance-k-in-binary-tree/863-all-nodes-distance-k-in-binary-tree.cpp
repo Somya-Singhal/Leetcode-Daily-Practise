3da4dc572613cf07176fc97c18304d238e99d2e6
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-17 08:19:18 +0530

    Time: 3 ms (93.01%), Space: 12.5 MB (76.59%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -56,49 +56,93 @@ public:
     // }
     
     //using map,dfs and bfs(graph)
-     unordered_map<int,vector<int>>mp;
-    void graph(TreeNode* child,TreeNode* parent)
+    //  unordered_map<int,vector<int>>mp;
+    // void graph(TreeNode* child,TreeNode* parent)
+    // {
+    //     if(parent && child)
+    //     {
+    //         mp[parent->val].push_back(child->val);
+    //         mp[child->val].push_back(parent->val);
+    //     }
+    //     if(child->left)
+    //         graph(child->left,child);
+    //     if(child->right)
+    //         graph(child->right,child);
+    // }
+    //  vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+    //     vector<int>res;
+    //     graph(root,NULL);
+    //      //bfs
+    //     queue<int>q;
+    //     unordered_set<int>vis;
+    //     q.push(target->val);
+    //     vis.insert(target->val);
+    //     for(int i=0;i<k;i++)
+    //     {
+    //         int len=q.size();
+    //         for(int j=0;j<len;j++)
+    //         {
+    //             int curr=q.front();
+    //             q.pop();
+    //             for(auto x:mp[curr])
+    //             {
+    //                 if(vis.count(x)==0)
+    //                 {
+    //                     q.push(x);
+    //                     vis.insert(x);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //      while(!q.empty())
+    //      {
+    //          res.push_back(q.front());
+    //          q.pop();
+    //      }
+    //     return res;
+    // }
+    
+    //without map
+    vector<int>res;
+    void dfs(TreeNode* root,int k,int depth)
     {
-        if(parent && child)
+        if(root==NULL) return;
+        if(depth==k)
+            res.push_back(root->val);
+        dfs(root->left,k,depth+1);
+        dfs(root->right,k,depth+1);
+    }
+    int path(TreeNode* root, TreeNode* target, int k)
+    {
+        if(root==NULL)
+            return -1;
+        else if(root==target)
         {
-            mp[parent->val].push_back(child->val);
-            mp[child->val].push_back(parent->val);
+            dfs(target,k,0);
+            return 1;
         }
-        if(child->left)
-            graph(child->left,child);
-        if(child->right)
-            graph(child->right,child);
+        else {
+        int l=path(root->left,target,k),r=path(root->right,target,k);
+        if(l!=-1)
+        {
+            if(l==k)
+                res.push_back(root->val);
+            dfs(root->right,k,l+1);
+            return l+1;
+        }
+        else if(r!=-1)
+        {
+            if(r==k)
+                res.push_back(root->val);
+            dfs(root->left,k,r+1);
+            return r+1;
+        }
+        else
+           return -1;
+        }
     }
      vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int>res;
-        graph(root,NULL);
-         //bfs
-        queue<int>q;
-        unordered_set<int>vis;
-        q.push(target->val);
-        vis.insert(target->val);
-        for(int i=0;i<k;i++)
-        {
-            int len=q.size();
-            for(int j=0;j<len;j++)
-            {
-                int curr=q.front();
-                q.pop();
-                for(auto x:mp[curr])
-                {
-                    if(vis.count(x)==0)
-                    {
-                        q.push(x);
-                        vis.insert(x);
-                    }
-                }
-            }
-        }
-         while(!q.empty())
-         {
-             res.push_back(q.front());
-             q.pop();
-         }
+        path(root,target,k);
         return res;
     }
 };
