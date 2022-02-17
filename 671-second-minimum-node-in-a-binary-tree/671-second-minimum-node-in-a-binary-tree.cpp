f523bc54a2ab6c0d1c97b08a4b56a94104755b80
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-17 09:40:41 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (22.56%) - LeetHub

--- 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.cpp
+++ 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.cpp
@@ -11,37 +11,62 @@
  */
 class Solution {
 public:
-    int findSecondMinimumValue(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        int first=-1,second=-1;
-        while(!q.empty())
-        {
-            int len=q.size();
-            for(int i=0;i<len;i++)
+    int first=-1,second=-1;
+    void findvalue(TreeNode* root)
+    {
+         if(root==NULL)
+            return;
+        if(first==-1)
+            first=root->val;
+        else{
+            if(root->val<first)
             {
-                TreeNode* curr=q.front();
-                if(first==-1)
-                    first=curr->val;
-                else
-                {
-                    if(curr->val<first)
-                    {
-                        second=first;
-                        first=curr->val;
-                    }
-                    else{
-                        if((second==-1 || second>curr->val) && curr->val!=first)
-                            second=curr->val;
-                    }
-                }
-                q.pop();
-                if(curr->left)
-                    q.push(curr->left);
-                if(curr->right)
-                    q.push(curr->right);
+                second=first;
+                first=root->val;
+            }
+            else{
+                if((second==-1 || second>root->val) && root->val!=first)
+                    second=root->val;
             }
         }
+        findvalue(root->left);
+        findvalue(root->right);
+    }
+    int findSecondMinimumValue(TreeNode* root) {
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // int first=-1,second=-1;
+        // while(!q.empty())
+        // {
+        //     int len=q.size();
+        //     for(int i=0;i<len;i++)
+        //     {
+        //         TreeNode* curr=q.front();
+        //         if(first==-1)
+        //             first=curr->val;
+        //         else
+        //         {
+        //             if(curr->val<first)
+        //             {
+        //                 second=first;
+        //                 first=curr->val;
+        //             }
+        //             else{
+        //                 if((second==-1 || second>curr->val) && curr->val!=first)
+        //                     second=curr->val;
+        //             }
+        //         }
+        //         q.pop();
+        //         if(curr->left)
+        //             q.push(curr->left);
+        //         if(curr->right)
+        //             q.push(curr->right);
+        //     }
+        // }
+        // return second;
+        if(root==NULL)
+            return -1;
+        findvalue(root);
         return second;
     }
 };
