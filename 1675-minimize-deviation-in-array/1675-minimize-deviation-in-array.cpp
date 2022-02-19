1d8e936a597f542f20af9d079eee8ae4c09e0318
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-19 08:41:54 +0530

    Time: 745 ms (13.90%), Space: 123.7 MB (9.27%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,31 +1,57 @@
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-        priority_queue<int>pq;
+        // priority_queue<int>pq;
+        // int n=nums.size();
+        // int currmin=INT_MAX;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]%2)
+        //     {
+        //         pq.push(nums[i]*2);
+        //         currmin=min(currmin,nums[i]*2);
+        //     }
+        //     else
+        //     {
+        //         pq.push(nums[i]);
+        //         currmin=min(currmin,nums[i]);
+        //     }
+        // }
+        // int currdev=INT_MAX;
+        // while(pq.top()%2==0)
+        // {
+        //     int t=pq.top();
+        //     pq.pop();
+        //     currdev=min(currdev,t-currmin);
+        //     pq.push(t/2);
+        //     currmin=min(currmin,t/2);
+        // }
+        // return currdev=min(currdev,pq.top()-currmin);
+        
+        multiset<int>s;
         int n=nums.size();
         int currmin=INT_MAX;
         for(int i=0;i<n;i++)
         {
             if(nums[i]%2)
             {
-                pq.push(nums[i]*2);
+                s.insert(nums[i]*2);
                 currmin=min(currmin,nums[i]*2);
             }
             else
             {
-                pq.push(nums[i]);
+                s.insert(nums[i]);
                 currmin=min(currmin,nums[i]);
             }
         }
         int currdev=INT_MAX;
-        while(pq.top()%2==0)
+        while(*s.rbegin()%2==0)
         {
-            int t=pq.top();
-            pq.pop();
-            currdev=min(currdev,t-currmin);
-            pq.push(t/2);
-            currmin=min(currmin,t/2);
+            int t=*s.rbegin();
+            s.erase(*s.rbegin());
+            currdev=min(currdev,t-*s.begin());
+            s.insert(t/2);
         }
-        return currdev=min(currdev,pq.top()-currmin);
+        return currdev=min(currdev,*s.rbegin()-*s.begin());
     }
 };
