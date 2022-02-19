9a059ccf2e7a9d7109fe2404395747b1a3dd718b
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-19 08:50:53 +0530

    Time: 321 ms (52.32%), Space: 64.3 MB (99.34%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -28,30 +28,48 @@ public:
         // }
         // return currdev=min(currdev,pq.top()-currmin);
         
-        multiset<int>s;
-        int n=nums.size();
-        int currmin=INT_MAX;
-        for(int i=0;i<n;i++)
+        // multiset<int>s;
+        // int n=nums.size();
+        // int currmin=INT_MAX;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]%2)
+        //     {
+        //         s.insert(nums[i]*2);
+        //         currmin=min(currmin,nums[i]*2);
+        //     }
+        //     else
+        //     {
+        //         s.insert(nums[i]);
+        //         currmin=min(currmin,nums[i]);
+        //     }
+        // }
+        // int currdev=INT_MAX;
+        // while(*s.rbegin()%2==0)
+        // {
+        //     int t=*s.rbegin();
+        //     s.erase(*s.rbegin());
+        //     currdev=min(currdev,t-*s.begin());
+        //     s.insert(t/2);
+        // }
+        // return currdev=min(currdev,*s.rbegin()-*s.begin());
+        
+       
+        int res=INT_MAX,min_n=INT_MAX;
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]%2)
-            {
-                s.insert(nums[i]*2);
-                currmin=min(currmin,nums[i]*2);
-            }
-            else
-            {
-                s.insert(nums[i]);
-                currmin=min(currmin,nums[i]);
-            }
+            nums[i]=nums[i]%2?nums[i]*2:nums[i];
+            min_n=min(min_n,nums[i]);
         }
-        int currdev=INT_MAX;
-        while(*s.rbegin()%2==0)
+        make_heap(begin(nums),end(nums));
+        while(nums[0]%2==0)
         {
-            int t=*s.rbegin();
-            s.erase(*s.rbegin());
-            currdev=min(currdev,t-*s.begin());
-            s.insert(t/2);
+            res=min(res,nums[0]-min_n);
+            min_n=min(min_n,nums[0]/2);
+            pop_heap(begin(nums),end(nums));
+            nums.back()/=2;
+            push_heap(begin(nums),end(nums));
         }
-        return currdev=min(currdev,*s.rbegin()-*s.begin());
+        return min(res,nums[0]-min_n);
     }
 };
