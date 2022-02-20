fa84b1e1ab7b52a52847ca09de5210325e15f8d5
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-21 01:57:28 +0530

    Time: 4 ms (92.45%), Space: 7.4 MB (56.21%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -43,7 +43,34 @@ public:
     //     return grid;
     // }
     
-    void solve(vector<vector<string>>& grid,vector<string>& res,vector<int>& flag_col,vector<int>& flag_45,vector<int>& flag_135,int row,int n)
+    // void solve(vector<vector<string>>& grid,vector<string>& res,vector<int>& flag_col,vector<int>& flag_45,vector<int>& flag_135,int row,int n)
+    // {
+    //     if(row==n)
+    //     {
+    //         grid.push_back(res);
+    //         return;
+    //     }
+    //     for(int col=0;col<n;col++)
+    //     {
+    //         if(flag_col[col] && flag_45[col+row] && flag_135[n-1+col-row])
+    //         {
+    //             flag_col[col]=flag_45[col+row]=flag_135[n-1+col-row]=0;
+    //             res[row][col]='Q';
+    //             solve(grid,res,flag_col,flag_45,flag_135,row+1,n);
+    //             res[row][col]='.';
+    //             flag_col[col]=flag_45[col+row]=flag_135[n-1+col-row]=1;
+    //         }
+    //     }
+    // }
+    // vector<vector<string>> solveNQueens(int n) {
+    //     vector<vector<string>>grid;
+    //     vector<string>res(n,string(n,'.'));
+    //     vector<int>flag_col(n,1),flag_45(2*n-1,1),flag_135(2*n-1,1);
+    //     solve(grid,res,flag_col,flag_45,flag_135,0,n);
+    //     return grid;
+    // }
+    
+     void solve(vector<vector<string>>& grid,vector<string>& res,vector<int>& flag,int row,int n)
     {
         if(row==n)
         {
@@ -52,21 +79,21 @@ public:
         }
         for(int col=0;col<n;col++)
         {
-            if(flag_col[col] && flag_45[col+row] && flag_135[n-1+col-row])
+            if(flag[col] && flag[n+col+row] && flag[4*n-2+col-row])
             {
-                flag_col[col]=flag_45[col+row]=flag_135[n-1+col-row]=0;
+                flag[col]=flag[n+col+row]=flag[4*n-2+col-row]=0;
                 res[row][col]='Q';
-                solve(grid,res,flag_col,flag_45,flag_135,row+1,n);
+                solve(grid,res,flag,row+1,n);
                 res[row][col]='.';
-                flag_col[col]=flag_45[col+row]=flag_135[n-1+col-row]=1;
+                flag[col]=flag[n+col+row]=flag[4*n-2+col-row]=1;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>grid;
         vector<string>res(n,string(n,'.'));
-        vector<int>flag_col(n,1),flag_45(2*n-1,1),flag_135(2*n-1,1);
-        solve(grid,res,flag_col,flag_45,flag_135,0,n);
+        vector<int>flag(5*n-2,1);
+        solve(grid,res,flag,0,n);
         return grid;
     }
 };
