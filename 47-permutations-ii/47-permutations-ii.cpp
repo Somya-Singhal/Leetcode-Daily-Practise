10bd751eddaa93af9396882048fe85a8a9471ca1
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-21 17:37:31 +0530

    Time: 4 ms (94.28%), Space: 8.6 MB (78.26%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,34 +1,64 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& res,int n,vector<int>& comb,unordered_map<int,int>& mp)
-    {
-        if(comb.size()==n)
-        {
-            res.push_back(comb);
-            return;
-        }
-        for(auto &x: mp){
-            int num=x.first;
-            int count=x.second;
-            if(count==0)
-                continue;
-            mp[x.first]--;
-            comb.push_back(x.first);
-            solve(res,n,comb,mp);
-            comb.pop_back();
-            mp[x.first]=count;
-        }
-    }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
+    // void solve(vector<vector<int>>& res,int n,vector<int>& comb,unordered_map<int,int>& mp)
+    // {
+    //     if(comb.size()==n)
+    //     {
+    //         res.push_back(comb);
+    //         return;
+    //     }
+    //     for(auto &x: mp){
+    //         int num=x.first;
+    //         int count=x.second;
+    //         if(count==0)
+    //             continue;
+    //         mp[x.first]--;
+    //         comb.push_back(x.first);
+    //         solve(res,n,comb,mp);
+    //         comb.pop_back();
+    //         mp[x.first]=count;
+    //     }
+    // }
+    // vector<vector<int>> permuteUnique(vector<int>& nums) {
+    //     int n=nums.size();
+    //     unordered_map<int,int>mp;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         mp[nums[i]]++;
+    //     }
+    //     vector<vector<int>>res;
+    //     vector<int>comb;
+    //     solve(res,n,comb,mp);
+    //     return res;
+    // }
+    
+     void solve(vector<vector<int>>& res,int n,vector<int>& comb,vector<int>& flag,vector<int>& nums)
+     {
+         if(comb.size()==n)
+         {
+             res.push_back(comb);
+             return;
+         }
+         for(int i=0;i<n;i++)
+         {
+             if(flag[i]==1)
+                 continue;
+             if(i>0 && nums[i-1]==nums[i] && flag[i-1]==0)
+                 continue;
+             flag[i]=1;
+             comb.push_back(nums[i]);
+             solve(res,n,comb,flag,nums);
+             flag[i]=0;
+             comb.pop_back();
+         }
+     }
+     vector<vector<int>> permuteUnique(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++)
-        {
-            mp[nums[i]]++;
-        }
+        sort(nums.begin(),nums.end());
         vector<vector<int>>res;
+        vector<int>flag(n,0);
         vector<int>comb;
-        solve(res,n,comb,mp);
+        solve(res,n,comb,flag,nums);
         return res;
     }
 };
