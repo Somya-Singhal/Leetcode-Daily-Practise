740486d114d4117d0afe9d8c15d2caf9b3a6663b
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-02-28 17:13:43 +0530

    Time: 3 ms (30.80%), Space: 6.8 MB (62.02%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -1,32 +1,58 @@
 class Solution {
 public:
     
+    // vector<string> summaryRanges(vector<int>& nums) {
+    //     vector<string>res;
+    //     int n=nums.size();
+    //     if(n==0)
+    //         return {};
+    //     int prev=nums[0];
+    //     for(int i=1;i<n;i++)
+    //     {
+    //         if(nums[i]==nums[i-1]+1)
+    //             continue;
+    //         else{
+    //             string str="";
+    //             if(prev==nums[i-1])
+    //                 str=to_string(prev);
+    //             else
+    //             str=to_string(prev)+"-"+">"+to_string(nums[i-1]);
+    //             res.push_back(str);
+    //             prev=nums[i];
+    //         }
+    //     }
+    //     if(prev==nums[n-1])
+    //             res.push_back(to_string(nums[n-1]));
+    //     else
+    //     {
+    //         string str=to_string(prev)+"-"+">"+to_string(nums[n-1]);
+    //         res.push_back(str);
+    //     }
+    //     return res;
+    // }
+    
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string>res;
         int n=nums.size();
-        if(n==0)
-            return {};
-        int prev=nums[0];
-        for(int i=1;i<n;i++)
+        if(n==1)
         {
-            if(nums[i]==nums[i-1]+1)
-                continue;
-            else{
-                string str="";
-                if(prev==nums[i-1])
-                    str=to_string(prev);
-                else
-                str=to_string(prev)+"-"+">"+to_string(nums[i-1]);
-                res.push_back(str);
-                prev=nums[i];
-            }
+            res.push_back(to_string(nums[0]));
+            return res;
         }
-        if(prev==nums[n-1])
-                res.push_back(to_string(nums[n-1]));
-        else
+        int start=0,end=0;
+        for(int i=0;i<n;i++)
         {
-            string str=to_string(prev)+"-"+">"+to_string(nums[n-1]);
-            res.push_back(str);
+            while(i<n-1 && nums[i]+1==nums[i+1])
+            {
+                i++;
+                end++;
+            }
+            if(start==end)
+                res.push_back(to_string(nums[start]));
+            else
+                res.push_back(to_string(nums[start])+"-"+">"+to_string(nums[end]));
+            end++;
+            start=end;
         }
         return res;
     }
