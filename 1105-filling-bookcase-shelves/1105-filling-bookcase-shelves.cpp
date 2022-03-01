83de65fce62da3190833ec80d5b7afa997611631
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-01 12:24:12 +0530

    Time: 15 ms (22.40%), Space: 12.7 MB (15.40%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-        int n=books.size();
-        vector<int>dp(n+1,0);
-        for(int i=1;i<=n;i++)
-        {
-            int currMaxHeight=books[i-1][1];
-            int width=books[i-1][0];
-            dp[i]=dp[i-1]+currMaxHeight;
-            for(int j=i-1;j>0 && width+books[j-1][0]<=shelfWidth;j--)
-            {
-                currMaxHeight=max(currMaxHeight,books[j-1][1]);
-                width+=books[j-1][0];
-                dp[i]=min(dp[i],dp[j-1]+currMaxHeight);
-            }
-        }
-        return dp[n];
+    // int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+    //     int n=books.size();
+    //     vector<int>dp(n+1,0);
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         int currMaxHeight=books[i-1][1];
+    //         int width=books[i-1][0];
+    //         dp[i]=dp[i-1]+currMaxHeight;
+    //         for(int j=i-1;j>0 && width+books[j-1][0]<=shelfWidth;j--)
+    //         {
+    //             currMaxHeight=max(currMaxHeight,books[j-1][1]);
+    //             width+=books[j-1][0];
+    //             dp[i]=min(dp[i],dp[j-1]+currMaxHeight);
+    //         }
+    //     }
+    //     return dp[n];
+    // }
+    
+     int dp[1001][1001]={};
+     int minHeightShelves(vector<vector<int>>& books, int shelfWidth,int idx=0,int wt=0,int maxht=0) {
+        if(idx>=books.size())
+            return maxht;
+        if(dp[idx][wt]!=0)
+            return dp[idx][wt];
+        return dp[idx][wt]=min(maxht+minHeightShelves(books,shelfWidth,idx+1,books[idx][0],books[idx][1]),wt+books[idx][0]>shelfWidth?INT_MAX:minHeightShelves(books,shelfWidth,idx+1,wt+books[idx][0],max(maxht,books[idx][1])));
     }
 };
