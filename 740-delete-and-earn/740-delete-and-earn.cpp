8b981c1afd6be2b5f209eda0b8f2a05678c6538e
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-05 11:45:14 +0530

    Time: 9 ms (63.92%), Space: 9.7 MB (67.05%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
-    int solve(unordered_map<int,int>& count,int maxnum,unordered_map<int,int>& cache)
-    {
-        if(maxnum==0)
-            return 0;
-        if(maxnum==1)
-            return count.find(1)!=count.end()?count[1]:0;
-        if(cache.find(maxnum)!=cache.end())
-            return cache[maxnum];
-        if(count.find(maxnum)!=count.end())
-        return cache[maxnum]=max(count[maxnum]+solve(count,maxnum-2,cache),solve(count,maxnum-1,cache));
-        return cache[maxnum]=solve(count,maxnum-1,cache);
-    }
-    int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int,int>count,cache;
+    //top-down-->time->O(N+k), spce->O(N+k)
+    // int solve(unordered_map<int,int>& count,int maxnum,unordered_map<int,int>& cache)
+    // {
+    //     if(maxnum==0)
+    //         return 0;
+    //     if(maxnum==1)
+    //         return count.find(1)!=count.end()?count[1]:0;
+    //     if(cache.find(maxnum)!=cache.end())
+    //         return cache[maxnum];
+    //     if(count.find(maxnum)!=count.end())
+    //     return cache[maxnum]=max(count[maxnum]+solve(count,maxnum-2,cache),solve(count,maxnum-1,cache));
+    //     return cache[maxnum]=solve(count,maxnum-1,cache);
+    // }
+    // int deleteAndEarn(vector<int>& nums) {
+    //     unordered_map<int,int>count,cache;
+    //     int n=nums.size();
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         count[nums[i]]++;
+    //     }
+    //     for(auto &x:count)
+    //     {
+    //         maxnum=max(maxnum,x.first);
+    //        x.second*=x.first;
+    //     }
+    //     return solve(count,maxnum,cache);
+    // }
+    
+    
+    //bottom-up--->
+     int deleteAndEarn(vector<int>& nums) {
+        unordered_map<int,int>count;
         int n=nums.size();
-        int maxnum=INT_MIN;
+         int maxnum=INT_MIN;
         for(int i=0;i<n;i++)
         {
             count[nums[i]]++;
@@ -25,6 +43,19 @@ public:
             maxnum=max(maxnum,x.first);
            x.second*=x.first;
         }
-        return solve(count,maxnum,cache);
+        vector<int>cache(maxnum+1,0);
+        if(count.find(1)!=count.end())
+        {
+            cache[1]=count[1];
+        }
+         for(int i=2;i<=maxnum;i++)
+         {
+             if(count.find(i)!=count.end())
+                 cache[i]=max(count[i]+cache[i-2],cache[i-1]);
+             else
+                 cache[i]=cache[i-1];
+         }
+        return cache[maxnum];
     }
+    
 };
