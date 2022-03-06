63b834e907b22223dfea35a06d59f42aef7dc902
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-06 14:15:35 +0530

    Time: 99 ms (6.60%), Space: 30.5 MB (8.18%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
-    long long cache[501][501]={0};
-    int mod=1e9+7;
-    long long solve(int unpicked,int undelivered)
-    {
-        if(unpicked==0 && undelivered==0)
-            return 1;
-        if(unpicked>undelivered || undelivered<0 || unpicked<0)
-            return 0;
-        if(cache[unpicked][undelivered]!=0)
-            return cache[unpicked][undelivered];
-        long long pickways=unpicked*solve(unpicked-1,undelivered)%mod;
-        long long deliveredways=(undelivered-unpicked)*solve(unpicked,undelivered-1)%mod;
-        return cache[unpicked][undelivered]=pickways+deliveredways;
-    }
-    int countOrders(int n) {
-       return solve(n,n); 
+    // long long cache[501][501]={0};
+    // int mod=1e9+7;
+    // long long solve(int unpicked,int undelivered)
+    // {
+    //     if(unpicked==0 && undelivered==0)
+    //         return 1;
+    //     if(unpicked>undelivered || undelivered<0 || unpicked<0)
+    //         return 0;
+    //     if(cache[unpicked][undelivered]!=0)
+    //         return cache[unpicked][undelivered];
+    //     long long pickways=unpicked*solve(unpicked-1,undelivered)%mod;
+    //     long long deliveredways=(undelivered-unpicked)*solve(unpicked,undelivered-1)%mod;
+    //     return cache[unpicked][undelivered]=pickways+deliveredways;
+    // }
+    // int countOrders(int n) {
+    //    return solve(n,n); 
+    // }
+    
+    
+     int mod=1e9+7;
+     int countOrders(int n) {
+      vector<vector<long long>>dp(n+1,vector<long long>(n+1,0));
+         for(int unpicked=0;unpicked<=n;unpicked++)
+         {
+             for(int undelivered=0;undelivered<=n;undelivered++)
+             {
+                 if(unpicked==0 && undelivered==0)
+                 {
+                     dp[unpicked][undelivered]=1;
+                     continue;
+                 }
+                 if(unpicked>0)
+                    dp[unpicked][undelivered]+=unpicked*dp[unpicked-1][undelivered]%mod;
+                 if(undelivered>unpicked)
+                 dp[unpicked][undelivered]+=(undelivered-unpicked)*dp[unpicked][undelivered-1]%mod;
+             }
+         }
+         return dp[n][n];
     }
 };
