46420b2de3688cc33cd3a3f27accc1b1f3b3b786
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-10 01:11:43 +0530

    Time: 12 ms (47.25%), Space: 11.2 MB (59.53%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -10,50 +10,73 @@
  */
 class Solution {
 public:
+    // ListNode* deleteDuplicates(ListNode* head) {
+    //     if(head==NULL || head->next==NULL)
+    //         return head;
+    //     ListNode *curr=head->next,*prev=head;
+    //     unordered_set<int>s;
+    //     while(curr!=NULL)
+    //     {
+    //         if(curr->val==prev->val)
+    //         {
+    //             ListNode *temp=curr;
+    //             s.insert(curr->val);
+    //             curr=curr->next;
+    //             prev->next=curr;
+    //             delete temp;
+    //         }
+    //         else
+    //         {
+    //             prev=prev->next;
+    //             curr=curr->next;
+    //         }
+    //     }
+    //     curr=head,prev=NULL;
+    //     while(curr!=NULL)
+    //     {
+    //         if(s.find(curr->val)!=s.end())
+    //         {
+    //             if(curr==head)
+    //             {
+    //                 head=head->next;
+    //                 prev=curr;
+    //                 curr=curr->next;
+    //             }
+    //             else
+    //             {
+    //                 curr=curr->next;
+    //                 prev->next=curr;
+    //             }
+    //         }
+    //         else
+    //         {
+    //             prev=curr;
+    //             curr=curr->next;   
+    //         }
+    //     }
+    //     return head;
+    // }
     ListNode* deleteDuplicates(ListNode* head) {
         if(head==NULL || head->next==NULL)
             return head;
-        ListNode *curr=head->next,*prev=head;
-        unordered_set<int>s;
-        while(curr!=NULL)
-        {
-            if(curr->val==prev->val)
-            {
-                ListNode *temp=curr;
-                s.insert(curr->val);
-                curr=curr->next;
-                prev->next=curr;
-                delete temp;
-            }
-            else
-            {
-                prev=prev->next;
-                curr=curr->next;
-            }
-        }
-        curr=head,prev=NULL;
-        while(curr!=NULL)
-        {
-            if(s.find(curr->val)!=s.end())
-            {
-                if(curr==head)
-                {
-                    head=head->next;
-                    prev=curr;
-                    curr=curr->next;
-                }
-                else
-                {
-                    curr=curr->next;
-                    prev->next=curr;
-                }
-            }
-            else
-            {
-                prev=curr;
-                curr=curr->next;   
-            }
-        }
-        return head;
+        ListNode *dummy=new ListNode(0);
+        ListNode *curr=dummy;
+        dummy->next=head;
+       while(head!=NULL)
+       {
+          if(head->next!=NULL && head->val==head->next->val)
+          {
+             while(head->next!=NULL && head->val==head->next->val)
+                 head=head->next;
+                  
+            curr->next=head->next;
+          }
+           else
+           {
+               curr=head;
+           }
+           head=head->next;
+       }
+        return dummy->next;
     }
 };
