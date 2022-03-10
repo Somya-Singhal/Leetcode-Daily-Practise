a32759f9b6296914bea3bbef170ad07b7d67544b
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-10 11:13:29 +0530

    Time: 66 ms (65.28%), Space: 32.6 MB (53.32%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int find(int i,vector<int>& parent)
+    {
+        if(parent[i]<0)
+            return i;
+        return parent[i]=find(parent[i],parent);
+    }
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int>parent(n,-1),indegree(n,0);
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(leftChild[i]!=-1)
+            {
+                int p=find(i,parent),c=find(leftChild[i],parent);
+                if(p!=c)
+                {
+                    parent[c]=p;
+                    indegree[leftChild[i]]++;
+                }
+                else
+                    return false;
+            }
+            if(rightChild[i]!=-1)
+            {
+                int p=find(i,parent),c=find(rightChild[i],parent);
+                if(p!=c)
+                {
+                    parent[c]=p;
+                    indegree[rightChild[i]]++;
+                }
+                else
+                    return false;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]>1)
+                return false;
+            if(indegree[i]==0)
+                count++;
+        }
+        return (count==1);
+    }
+};
