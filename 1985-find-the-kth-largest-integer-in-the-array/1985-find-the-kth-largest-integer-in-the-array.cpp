d8fab687d2521e07a0597879cc43aaab5c6ad655
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-10 07:52:03 +0530

    Time: 229 ms (87.06%), Space: 55.2 MB (80.20%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,27 +1,27 @@
-// class mycomp {
-//     public:
-//     bool operator()(string &s1,string &s2)
-//     {
-//         if(s1.size()!=s2.size())
-//             return s1.size()>s2.size();
-//         return s1>s2;
-//     }
-// };
-
-class mycomp {
+class mymincomp {
     public:
     bool operator()(string &s1,string &s2)
     {
         if(s1.size()!=s2.size())
-            return s1.size()<s2.size();
-        return s1<s2;
+            return s1.size()>s2.size();
+        return s1>s2;
     }
 };
+
+// class mymaxcomp {
+//     public:
+//     bool operator()(string &s1,string &s2)
+//     {
+//         if(s1.size()!=s2.size())
+//             return s1.size()<s2.size();
+//         return s1<s2;
+//     }
+// };
 class Solution {
 public:
     //min-heap->O(nlogk*m)
     // string kthLargestNumber(vector<string>& nums, int k) {
-    //     priority_queue<string,vector<string>,mycomp>pq;
+    //     priority_queue<string,vector<string>,mymincomp>pq;
     //     int n=nums.size();
     //     for(int i=0;i<n;i++)
     //     {
@@ -33,13 +33,20 @@ public:
     // }
     
     //max-heap->O((n+klogn)*m)
+    // string kthLargestNumber(vector<string>& nums, int k) {
+    //     make_heap(nums.begin(),nums.end(),mymaxcomp());
+    //     while(k-->1)
+    //     {
+    //         pop_heap(nums.begin(),nums.end(),mycomp());
+    //         nums.pop_back();
+    //     }
+    //     return nums.front();
+    // }
+    
+    //time->O(n*m)
     string kthLargestNumber(vector<string>& nums, int k) {
-        make_heap(nums.begin(),nums.end(),mycomp());
-        while(k-->1)
-        {
-            pop_heap(nums.begin(),nums.end(),mycomp());
-            nums.pop_back();
-        }
-        return nums.front();
+        nth_element(nums.begin(),nums.begin()+k-1,nums.end(),mymincomp());
+        return nums[k-1];
     }
+    
 };
