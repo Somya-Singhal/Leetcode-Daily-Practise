f049e278de66db72516189eba1f690666bb082ff
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-10 07:49:28 +0530

    Time: 258 ms (80.57%), Space: 55.2 MB (92.00%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,24 +1,45 @@
+// class mycomp {
+//     public:
+//     bool operator()(string &s1,string &s2)
+//     {
+//         if(s1.size()!=s2.size())
+//             return s1.size()>s2.size();
+//         return s1>s2;
+//     }
+// };
+
 class mycomp {
     public:
     bool operator()(string &s1,string &s2)
     {
         if(s1.size()!=s2.size())
-            return s1.size()>s2.size();
-        return s1>s2;
+            return s1.size()<s2.size();
+        return s1<s2;
     }
 };
-
 class Solution {
 public:
+    //min-heap->O(nlogk*m)
+    // string kthLargestNumber(vector<string>& nums, int k) {
+    //     priority_queue<string,vector<string>,mycomp>pq;
+    //     int n=nums.size();
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         pq.push(nums[i]);
+    //         if(pq.size()>k)
+    //             pq.pop();
+    //     }
+    //     return pq.top();
+    // }
+    
+    //max-heap->O((n+klogn)*m)
     string kthLargestNumber(vector<string>& nums, int k) {
-        priority_queue<string,vector<string>,mycomp>pq;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
+        make_heap(nums.begin(),nums.end(),mycomp());
+        while(k-->1)
         {
-            pq.push(nums[i]);
-            if(pq.size()>k)
-                pq.pop();
+            pop_heap(nums.begin(),nums.end(),mycomp());
+            nums.pop_back();
         }
-        return pq.top();
+        return nums.front();
     }
 };
