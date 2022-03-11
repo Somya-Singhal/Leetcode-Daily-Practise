b8b7ea0b24481ebd967c87446671f98abca85ecb
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-11 21:55:44 +0530

    Time: 5 ms (16.68%), Space: 6.5 MB (94.84%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -1,10 +1,53 @@
 class Solution {
 public:
-    int minMutation(string start, string end, vector<string>& bank) {
+    // int minMutation(string start, string end, vector<string>& bank) {
+    //     if(start.length()!=end.length())
+    //         return -1;
+    //     unordered_set<string>word;
+    //     unordered_set<string>vis;
+    //     vector<char>list{'A','C','G','T'};
+    //     for(auto x: bank)
+    //     {
+    //         word.insert({x});
+    //     }
+    //     int level=0;
+    //     queue<string>q;
+    //     vis.insert({start});
+    //     q.push(start);
+    //     while(!q.empty())
+    //     {
+    //         int len=q.size();
+    //         for(int l=0;l<len;l++) 
+    //         {
+    //             string curr=q.front();
+    //             q.pop();
+    //             if(curr==end)
+    //                 return level;
+    //             string temp=curr;
+    //             for(int i=0;i<temp.length();i++)
+    //             {
+    //                 for(int j=0;j<list.size();j++)
+    //                 {
+    //                     temp[i]=list[j];
+    //                     if(vis.find(temp)==vis.end() && word.find(temp)!=word.end())
+    //                     {
+    //                         vis.insert({temp});
+    //                         q.push(temp);
+    //                     }
+    //                     temp[i]=curr[i];
+    //                 }
+    //             }
+    //          }
+    //         level++;
+    //     }
+    //     return -1;
+    // }
+    
+    //some memory saved
+     int minMutation(string start, string end, vector<string>& bank) {
         if(start.length()!=end.length())
             return -1;
         unordered_set<string>word;
-        unordered_set<string>vis;
         vector<char>list{'A','C','G','T'};
         for(auto x: bank)
         {
@@ -12,7 +55,6 @@ public:
         }
         int level=0;
         queue<string>q;
-        vis.insert({start});
         q.push(start);
         while(!q.empty())
         {
@@ -29,9 +71,9 @@ public:
                     for(int j=0;j<list.size();j++)
                     {
                         temp[i]=list[j];
-                        if(vis.find(temp)==vis.end() && word.find(temp)!=word.end())
+                        if(word.find(temp)!=word.end())
                         {
-                            vis.insert({temp});
+                            word.erase(temp);
                             q.push(temp);
                         }
                         temp[i]=curr[i];
