274b54f57c2c79833f98c8bae4d72fe98ca7b964
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-11 21:48:23 +0530

    Time: 4 ms (21.73%), Space: 6.6 MB (75.85%) - LeetHub

---
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        if(start.length()!=end.length())
+            return -1;
+        unordered_set<string>word;
+        unordered_set<string>vis;
+        vector<char>list{'A','C','G','T'};
+        for(auto x: bank)
+        {
+            word.insert({x});
+        }
+        int level=0;
+        queue<string>q;
+        vis.insert({start});
+        q.push(start);
+        while(!q.empty())
+        {
+            int len=q.size();
+            for(int l=0;l<len;l++) 
+            {
+                string curr=q.front();
+                q.pop();
+                if(curr==end)
+                    return level;
+                string temp=curr;
+                for(int i=0;i<temp.length();i++)
+                {
+                    for(int j=0;j<list.size();j++)
+                    {
+                        temp[i]=list[j];
+                        if(vis.find(temp)==vis.end() && word.find(temp)!=word.end())
+                        {
+                            vis.insert({temp});
+                            q.push(temp);
+                        }
+                        temp[i]=curr[i];
+                    }
+                }
+             }
+            level++;
+        }
+        return -1;
+    }
+};
