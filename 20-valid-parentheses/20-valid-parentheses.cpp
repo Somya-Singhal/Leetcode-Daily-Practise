6c813de78529ad8170b4932612e5fc8cac6414fa
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-12 17:46:45 +0530

    Time: 3 ms (35.90%), Space: 6.2 MB (80.15%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       stack<char>st;
+       if(s[0]==']' || s[0]=='}' || s[0]==')')
+           return false;
+        st.push(s[0]);
+        for(int i=1;i<s.length();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+               st.push(s[i]);
+            else
+            {
+                if(st.empty() || (s[i]==')' && st.top()!='(') || (s[i]=='}' && st.top()!='{') || (s[i]==']' && st.top()!='['))
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
