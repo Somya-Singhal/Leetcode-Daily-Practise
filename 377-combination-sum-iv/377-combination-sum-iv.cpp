d39d1d2449715bb8c913c5d2679789d7b56e5c2f
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-15 13:15:00 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (67.84%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
-    int totalWays(vector<int>& nums,int target,unordered_map<int,int>& m)
-    {
-        if(target==0)
-        {
-            return 1;
-        }
-        if(target<0)
-            return 0;
-        if(m.find(target)!=m.end())
-        {
-            return m[target];
-        }
-        int count=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>target)
-                break;
-            count+=totalWays(nums,target-nums[i],m);
-        }
-        return m[target]=count;
-    }
-    int combinationSum4(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        unordered_map<int,int>m;
-        return totalWays(nums,target,m);
+    // int totalWays(vector<int>& nums,int target,unordered_map<int,int>& m)
+    // {
+    //     if(target==0)
+    //     {
+    //         return 1;
+    //     }
+    //     if(target<0)
+    //         return 0;
+    //     if(m.find(target)!=m.end())
+    //     {
+    //         return m[target];
+    //     }
+    //     int count=0;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         if(nums[i]>target)
+    //             break;
+    //         count+=totalWays(nums,target-nums[i],m);
+    //     }
+    //     return m[target]=count;
+    // }
+    // int combinationSum4(vector<int>& nums, int target) {
+    //     sort(nums.begin(),nums.end());
+    //     unordered_map<int,int>m;
+    //     return totalWays(nums,target,m);
+    // }
+    
+     int totalWays(vector<int>& nums,int target,vector<int>& dp)
+     {
+         if(target==0)
+             return 1;
+         if(dp[target]!=-1)
+             return dp[target];
+         int count=0;
+         for(int i=0;i<nums.size();i++)
+         {
+             if(nums[i]<=target)
+              count+=totalWays(nums,target-nums[i],dp);
+         }
+        return dp[target]=count;
+     }
+     int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+1,-1);
+        dp[0]=1; 
+        return totalWays(nums,target,dp);
     }
 };
