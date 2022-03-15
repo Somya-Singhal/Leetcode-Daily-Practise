f1b9c5d1e8bff392e4ffd154f09eb4d860815435
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-15 13:28:09 +0530

    Time: 4 ms (40.22%), Space: 6.7 MB (32.27%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -27,23 +27,40 @@ public:
     //     return totalWays(nums,target,m);
     // }
     
-     int totalWays(vector<int>& nums,int target,vector<int>& dp)
-     {
+    //  int totalWays(vector<int>& nums,int target,vector<int>& dp)
+    //  {
+    //      if(target==0)
+    //          return 1;
+    //      if(dp[target]!=-1)
+    //          return dp[target];
+    //      int count=0;
+    //      for(int i=0;i<nums.size();i++)
+    //      {
+    //          if(nums[i]<=target)
+    //           count+=totalWays(nums,target-nums[i],dp);
+    //      }
+    //     return dp[target]=count;
+    //  }
+    //  int combinationSum4(vector<int>& nums, int target) {
+    //     vector<int>dp(target+1,-1);
+    //     dp[0]=1; 
+    //     return totalWays(nums,target,dp);
+    // }
+    
+    
+     int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned long long>dp(target+1,0);
+        dp[0]=1; 
          if(target==0)
              return 1;
-         if(dp[target]!=-1)
-             return dp[target];
-         int count=0;
-         for(int i=0;i<nums.size();i++)
+         for(int i=1;i<=target;i++)
          {
-             if(nums[i]<=target)
-              count+=totalWays(nums,target-nums[i],dp);
+             for(int j=0;j<nums.size();j++)
+             {
+                if(i>=nums[j])
+                dp[i]+=dp[i-nums[j]];
+             }
          }
-        return dp[target]=count;
-     }
-     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,-1);
-        dp[0]=1; 
-        return totalWays(nums,target,dp);
+        return dp[target];
     }
 };
