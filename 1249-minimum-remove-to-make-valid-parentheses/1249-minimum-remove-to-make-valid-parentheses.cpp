19ee3b0463a239747dd86befb8c20d29e0fca200
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-15 11:56:21 +0530

    Time: 18 ms (93.67%), Space: 10.8 MB (77.91%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,28 +1,65 @@
 class Solution {
 public:
         
-    string minRemoveToMakeValid(string s) {
-        int n=s.length();
-        string str1=s,str2=s;
-        stack<int>st;
-        for(int i=0;i<n;i++)
-        {
-           if(s[i]=='(')
-               st.push(i);
-            if(s[i]==')')
-            {
-                if(!st.empty())
-                    st.pop();
-                else
-                    s[i]='*';
-            }
-        }
-        while(!st.empty())
-        {
-            s[st.top()]='*';
-            st.pop();
-        }
-        s.erase(remove(s.begin(),s.end(),'*'),s.end());
-        return s;
-    }
+    // string minRemoveToMakeValid(string s) {
+    //     int n=s.length();
+    //     string str1=s,str2=s;
+    //     stack<int>st;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //        if(s[i]=='(')
+    //            st.push(i);
+    //         if(s[i]==')')
+    //         {
+    //             if(!st.empty())
+    //                 st.pop();
+    //             else
+    //                 s[i]='*';
+    //         }
+    //     }
+    //     while(!st.empty())
+    //     {
+    //         s[st.top()]='*';
+    //         st.pop();
+    //     }
+    //     s.erase(remove(s.begin(),s.end(),'*'),s.end());
+    //     return s;
+    // }
+    
+     string minRemoveToMakeValid(string s) {
+         int n=s.length();
+         int count=0;
+         for(int i=0;i<n;i++)
+         {
+             if(s[i]=='(')
+                 count++;
+             else if(s[i]==')')
+             {
+                 if(count==0)
+                     s[i]='*';
+                 else
+                     count--;
+             }
+         }
+         count=0;
+         for(int i=n-1;i>=0;i--)
+         {
+             if(s[i]==')')
+                 count++;
+             else if(s[i]=='(')
+             {
+                 if(count==0)
+                     s[i]='*';
+                 else
+                     count--;
+             }
+         }
+         string ans="";
+         for(int i=0;i<n;i++)
+         {
+             if(s[i]!='*')
+                 ans+=s[i];
+         }
+         return ans;
+     }
 };
