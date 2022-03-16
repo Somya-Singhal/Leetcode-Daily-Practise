d721f3e204a1cb6fcb5992cf225416e7872c5ac8
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-16 18:20:02 +0530

    Time: 8 ms (83.81%), Space: 15.5 MB (15.68%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,41 +1,57 @@
 class Solution {
 public:
-    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int n1=pushed.size(),n2=popped.size();
+    // bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+    //     int n1=pushed.size(),n2=popped.size();
+    //     stack<int>s;
+    //     unordered_set<int>st;
+    //     int i=0,j=0;
+    //     while(i<n1)
+    //     {
+    //         if(st.find(popped[j])!=st.end())
+    //         {
+    //             if(s.top()==popped[j])
+    //                 s.pop();
+    //             else
+    //                 return false;
+    //             j++;
+    //         }
+    //         else if(pushed[i]==popped[j])
+    //         {
+    //             i++,j++;
+    //             continue;
+    //         }
+    //         else if(pushed[i]!=popped[j] && st.find(popped[j])==st.end())
+    //         {
+    //             s.push(pushed[i]);
+    //             st.insert(pushed[i]);
+    //             i++;
+    //         }
+    //     }
+    //     while(j<n2)
+    //     {
+    //         if(s.empty())
+    //             return false;
+    //         if(s.top()!=popped[j])
+    //             return false;
+    //         s.pop();
+    //         j++;
+    //     }
+    //     return true;
+    // }
+    
+     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n=pushed.size();
         stack<int>s;
-        unordered_set<int>st;
-        int i=0,j=0;
-        while(i<n1)
+         int j=0;
+        for(int i=0;i<n;i++)
         {
-            if(st.find(popped[j])!=st.end())
+            s.push(pushed[i]);
+            while(!s.empty() && popped[j]==s.top() && j<n)
             {
-                if(s.top()==popped[j])
-                    s.pop();
-                else
-                    return false;
+                s.pop();
                 j++;
             }
-            else if(pushed[i]==popped[j])
-            {
-                i++,j++;
-                continue;
-            }
-            else if(pushed[i]!=popped[j] && st.find(popped[j])==st.end())
-            {
-                s.push(pushed[i]);
-                st.insert(pushed[i]);
-                i++;
-            }
         }
-        while(j<n2)
-        {
-            if(s.empty())
-                return false;
-            if(s.top()!=popped[j])
-                return false;
-            s.pop();
-            j++;
-        }
-        return true;
+         return j==n;
     }
 };
