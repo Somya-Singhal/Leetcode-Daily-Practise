4b3f3fad744246bc2f20bbb00a4192eddf03492f
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-16 21:15:45 +0530

    Time: 8 ms (73.84%), Space: 8.3 MB (60.22%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,42 +1,62 @@
 class Solution {
 public:
-    string getHappyString(int n, int k) {
-        queue<string>q;
-        q.push("a");
-        q.push("b");
-        q.push("c");
-        while(!q.empty())
-        {
-            int len=q.size();
-            for(int i=0;i<len;i++)
-            {
-                string str=q.front();
-                int s=str.length();
-                q.pop();
-                if(s>n)
-                    return "";
-                if(s==n)
-                    k--;
-                if(k==0)
-                    return str;
+//     string getHappyString(int n, int k) {
+//         queue<string>q;
+//         q.push("a");
+//         q.push("b");
+//         q.push("c");
+//         while(!q.empty())
+//         {
+//             int len=q.size();
+//             for(int i=0;i<len;i++)
+//             {
+//                 string str=q.front();
+//                 int s=str.length();
+//                 q.pop();
+//                 if(s>n)
+//                     return "";
+//                 if(s==n)
+//                     k--;
+//                 if(k==0)
+//                     return str;
                 
-                if(str[s-1]=='a')
-                {
-                    q.push(str+'b');
-                    q.push(str+'c');
-                }
-                else if(str[s-1]=='b')
-                {
-                    q.push(str+'a');
-                    q.push(str+'c');
-                }
-                else
-                {
-                    q.push(str+'a');
-                    q.push(str+'b');
-                }
-            }
+//                 if(str[s-1]=='a')
+//                 {
+//                     q.push(str+'b');
+//                     q.push(str+'c');
+//                 }
+//                 else if(str[s-1]=='b')
+//                 {
+//                     q.push(str+'a');
+//                     q.push(str+'c');
+//                 }
+//                 else
+//                 {
+//                     q.push(str+'a');
+//                     q.push(str+'b');
+//                 }
+//             }
+//         }
+//         return "";
+//     }
+    
+    vector<string>res;
+    void solve(string ans,int n,int k)
+    {
+        if(res.size()==k)
+            return;
+        if(ans.length()>n)
+            return;
+        if(ans.length()==n)
+            res.push_back(ans);
+        for(char ch='a';ch<='c';ch++)
+        {
+            if(ans=="" || (ans.size()>0 && ch!=ans[ans.length()-1]))
+                solve(ans+ch,n,k);
         }
-        return "";
+    }
+    string getHappyString(int n, int k) {
+        solve("",n,k);
+        return res.size()==k?res[k-1]:"";
     }
 };
