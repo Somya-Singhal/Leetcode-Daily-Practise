ab81b4a9a2860047c12dd7e1caabc5b56b436b38
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-16 16:17:11 +0530

    Time: 41 ms (5.05%), Space: 16.2 MB (15.68%) - LeetHub

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n1=pushed.size(),n2=popped.size();
+        stack<int>s;
+        unordered_set<int>st;
+        int i=0,j=0;
+        while(i<n1)
+        {
+            if(st.find(popped[j])!=st.end())
+            {
+                if(s.top()==popped[j])
+                    s.pop();
+                else
+                    return false;
+                j++;
+            }
+            else if(pushed[i]==popped[j])
+            {
+                i++,j++;
+                continue;
+            }
+            else if(pushed[i]!=popped[j] && st.find(popped[j])==st.end())
+            {
+                s.push(pushed[i]);
+                st.insert(pushed[i]);
+                i++;
+            }
+        }
+        while(j<n2)
+        {
+            if(s.empty())
+                return false;
+            if(s.top()!=popped[j])
+                return false;
+            s.pop();
+            j++;
+        }
+        return true;
+    }
+};
