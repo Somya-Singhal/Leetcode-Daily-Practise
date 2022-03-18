26d0f77c6132cd9c2030fdebc63ced3f3c3830ca
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-18 07:47:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (7.98%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,18 +1,49 @@
 class Solution {
 public:
-    int scoreOfParentheses(string s) {
-        stack<int> st;
-        int score = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '('){
-                st.push(score);
-                score = 0;
-            }
-            else {
-                score = st.top() + max(2 * score, 1);
-                st.pop();
-            }
-        }
-        return score;
-    }
+    // int scoreOfParentheses(string s) {
+    //     stack<int> st;
+    //     int score = 0;
+    //     for(int i = 0; i < s.size(); i++){
+    //         if(s[i] == '('){
+    //             st.push(score);
+    //             score = 0;
+    //         }
+    //         else {
+    //             score = st.top() + max(2 * score, 1);
+    //             st.pop();
+    //         }
+    //     }
+    //     return score;
+    // }
+    
+     int scoreOfParentheses(string s) {
+         stack<int>st;
+         int n=s.length();
+         for(int i=0;i<n;i++)
+         {
+             if(s[i]=='(')
+                 st.push(0);
+             else if(st.top()==0) {
+                 st.pop();
+                 st.push(1);
+             }
+             else{
+                 int sum=0;
+                 while(st.top()!=0)
+                 {
+                     sum+=st.top();
+                     st.pop();
+                 }
+                     st.pop();
+                     st.push(sum*2);
+             }
+         }
+         int ans=0;
+         while(!st.empty())
+         {
+             ans+=st.top();
+             st.pop();
+         }
+         return ans;
+     }
 };
