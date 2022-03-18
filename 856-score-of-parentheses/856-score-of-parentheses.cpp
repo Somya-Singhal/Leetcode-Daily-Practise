119d9b2459e1213b65570196f692fc7deca95170
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-18 08:21:14 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (83.00%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -16,33 +16,49 @@ public:
     //     return score;
     // }
     
+     // int scoreOfParentheses(string s) {
+     //     stack<int>st;
+     //     int n=s.length();
+     //     for(int i=0;i<n;i++)
+     //     {
+     //         if(s[i]=='(')
+     //             st.push(0);
+     //         else if(st.top()==0) {
+     //             st.pop();
+     //             st.push(1);
+     //         }
+     //         else{
+     //             int sum=0;
+     //             while(st.top()!=0)
+     //             {
+     //                 sum+=st.top();
+     //                 st.pop();
+     //             }
+     //                 st.pop();
+     //                 st.push(sum*2);
+     //         }
+     //     }
+     //     int ans=0;
+     //     while(!st.empty())
+     //     {
+     //         ans+=st.top();
+     //         st.pop();
+     //     }
+     //     return ans;
+     // }
+    
      int scoreOfParentheses(string s) {
-         stack<int>st;
          int n=s.length();
+         int ans=0,bal=0;
          for(int i=0;i<n;i++)
          {
              if(s[i]=='(')
-                 st.push(0);
-             else if(st.top()==0) {
-                 st.pop();
-                 st.push(1);
-             }
+                 bal++;
              else{
-                 int sum=0;
-                 while(st.top()!=0)
-                 {
-                     sum+=st.top();
-                     st.pop();
-                 }
-                     st.pop();
-                     st.push(sum*2);
+                 bal--;
+                 if(s[i-1]=='(')
+                     ans+=1<<bal;
              }
-         }
-         int ans=0;
-         while(!st.empty())
-         {
-             ans+=st.top();
-             st.pop();
          }
          return ans;
      }
