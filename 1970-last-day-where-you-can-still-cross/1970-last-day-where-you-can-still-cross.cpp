40250ed5f044b393e044be318031482e675e3c23
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-03-24 03:09:46 +0530

    Time: 1196 ms (21.99%), Space: 188.4 MB (47.85%) - LeetHub

---
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    // int dx[4]={0,-1,0,1};
+    // int dy[4]={-1,0,1,0};
+    int DIR[5] = {0, 1, 0, -1, 0};
+   
+    bool solve(vector<vector<int>>& cells,int row,int col,int mid)
+    {
+        vector<vector<bool>>grid(row,vector<bool>(col,0));
+        queue<pair<int,int>>q;
+        for(int i=0;i<mid;i++)
+        {
+            grid[cells[i][0]-1][cells[i][1]-1]=1;
+        }
+        for(int i=0;i<col;i++)
+        {
+            if(grid[0][i]==0)
+            {
+                 q.push({0,i});
+                 grid[0][i]=1;
+            }
+        }
+        while(!q.empty())
+        {
+           int x=q.front().first,y=q.front().second;
+            q.pop();
+            if(x==row-1)
+                return true;
+            for(int i=0;i<4;i++)
+            {
+                int newx=x+DIR[i],newy=y+DIR[i+1];
+                if( newx<0 || newx==row || newy<0 || newy==col || grid[newx][newy]==1)
+                    continue;
+                grid[newx][newy]=1;
+                q.push({newx,newy});
+            }
+        }
+        return false;
+    }
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        int n=cells.size();
+        int lo=1,hi=n;
+        int lastDay=0;
+        while(lo<=hi)
+        {
+            int mid=lo+(hi-lo)/2;
+            if(solve(cells,row,col,mid))
+            {
+                 lastDay=mid;
+                 lo=mid+1;
+               
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+        return lastDay;
+    }
+};
