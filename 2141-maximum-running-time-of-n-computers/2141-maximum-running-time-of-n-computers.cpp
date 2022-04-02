3020e7a957753fc46c45896168e630c3eb653874
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-04-02 10:13:25 +0530

    Time: 136 ms (95.10%), Space: 55.8 MB (57.44%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
-    bool isTrue(vector<int>& batteries,long long t,int computers) {
-        int n=batteries.size();
-        long long sum=0;
-        for(int i=0;i<n;i++){
-            if(batteries[i]<=t)
-                sum+=batteries[i];
-            else
-                sum+=t;
-        }
-        return sum>=computers*t;
-    }
-    long long maxRunTime(int n, vector<int>& batteries) {
+    // bool isTrue(vector<int>& batteries,long long t,int computers) {
+    //     int n=batteries.size();
+    //     long long sum=0;
+    //     for(int i=0;i<n;i++){
+    //         if(batteries[i]<=t)
+    //             sum+=batteries[i];
+    //         else
+    //             sum+=t;
+    //     }
+    //     return sum>=computers*t;
+    // }
+    // long long maxRunTime(int n, vector<int>& batteries) {
+    //     int len=batteries.size();
+    //     long long sum=0,ans=0;
+    //     for(int i=0;i<len;i++)
+    //     {
+    //         sum+=batteries[i];
+    //     }
+    //     long long lo=0,hi=sum;
+    //     while(lo<=hi){
+    //         long long maxTime=(lo+hi)/2;
+    //         if(isTrue(batteries,maxTime,n)){
+    //             ans=maxTime;
+    //             lo=maxTime+1;
+    //         } else{
+    //             hi=maxTime-1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+     long long maxRunTime(int n, vector<int>& batteries) {
         int len=batteries.size();
+        sort(batteries.begin(),batteries.end());
         long long sum=0,ans=0;
         for(int i=0;i<len;i++)
         {
             sum+=batteries[i];
         }
-        long long lo=0,hi=sum;
-        while(lo<=hi){
-            long long maxTime=(lo+hi)/2;
-            if(isTrue(batteries,maxTime,n)){
-                ans=maxTime;
-                lo=maxTime+1;
-            } else{
-                hi=maxTime-1;
-            }
+        int l=0;
+        while(batteries[len-1-l]>sum/(n-l))
+        {
+            sum-=batteries[len-1-l++];
         }
-        return ans;
+         return sum/(n-l);
     }
 };
