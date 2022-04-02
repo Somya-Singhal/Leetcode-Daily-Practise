47f55b95716fc1f6a21e803f898b4f5b3ec26e5e
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-04-02 12:43:05 +0530

    Time: 641 ms (12.56%), Space: 64.4 MB (14.88%) - LeetHub

--- 956-tallest-billboard/956-tallest-billboard.cpp
+++ 956-tallest-billboard/956-tallest-billboard.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    int solve(int i,int s1,int s2,int target,vector<vector<int>>& m,vector<int>& rods)
-    {
-        if(s1>target || s2>target)
-            return -1;
-        if(i>=rods.size())
-            return s1==s2?s1:-1;
-        if(m[i][abs(s1-s2)]==-2)
-        {
-           m[i][abs(s1-s2)]=max(-1,max({solve(i+1,s1,s2,target,m,rods),solve(i+1,s1+rods[i],s2,target,m,rods),solve(i+1,s1,s2+rods[i],target,m,rods)})-max(s1,s2)); 
-        }
-        return m[i][abs(s1-s2)]+(m[i][abs(s1-s2)]==-1?0:max(s1,s2));
-    }
-    int tallestBillboard(vector<int>& rods) {
-        int sum=0,n=rods.size();
-        for(int i=0;i<n;i++)
-        {
-            sum+=rods[i];
-        }
-        int target=sum/2;
-        vector<vector<int>>m(n,vector<int>(target+1,-2));
-        return max(0,solve(0,0,0,target,m,rods));
+    // int solve(int i,int s1,int s2,int target,vector<vector<int>>& m,vector<int>& rods)
+    // {
+    //     if(s1>target || s2>target)
+    //         return -1;
+    //     if(i>=rods.size())
+    //         return s1==s2?s1:-1;
+    //     if(m[i][abs(s1-s2)]==-2)
+    //     {
+    //        m[i][abs(s1-s2)]=max(-1,max({solve(i+1,s1,s2,target,m,rods),solve(i+1,s1+rods[i],s2,target,m,rods),solve(i+1,s1,s2+rods[i],target,m,rods)})-max(s1,s2)); 
+    //     }
+    //     return m[i][abs(s1-s2)]+(m[i][abs(s1-s2)]==-1?0:max(s1,s2));
+    // }
+    // int tallestBillboard(vector<int>& rods) {
+    //     int sum=0,n=rods.size();
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         sum+=rods[i];
+    //     }
+    //     int target=sum/2;
+    //     vector<vector<int>>m(n,vector<int>(target+1,-2));
+    //     return max(0,solve(0,0,0,target,m,rods));
+    // }
+    
+     int tallestBillboard(vector<int>& rods) {
+        unordered_map<int,int>dp;
+         dp[0]=0;
+         for(int i=0;i<rods.size();i++){
+             unordered_map<int,int>cnt(dp);
+             for(auto x: cnt){
+                 int diff=x.first;
+                 dp[diff+rods[i]]=max(dp[diff+rods[i]],cnt[diff]);
+                 dp[abs(diff-rods[i])]=max(dp[abs(diff-rods[i])],cnt[diff]+min(rods[i],diff));
+             }
+         }
+         return dp[0];
     }
 };
