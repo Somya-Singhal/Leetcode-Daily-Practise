a0b23f5bdef5b6a7699fdaac7255872c7ef1f012
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-04-04 17:14:05 +0530

    Time: 482 ms (20.00%), Space: 84.4 MB (60.00%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,26 +1,51 @@
 class Solution {
 public:
   
+    // int maximumCandies(vector<int>& candies, long long k) {
+    //     int n=candies.size();
+    //     long long sum=0;
+    //     for(int i=0;i<n;i++)
+    //         sum+=candies[i];
+    //     long long lo=0,hi=sum;
+    //     while(lo<hi)
+    //     {
+    //         long long mid=(lo+hi+1)/2,num=0;
+    //         for(int i=0;i<n && num<k;i++)
+    //         {
+    //             num+=candies[i]/mid;
+    //         }
+    //         if(num>=k)
+    //         {
+    //             lo=mid;
+    //         }
+    //         else
+    //             hi=mid-1;
+    //     }
+    //     return lo;
+    // }
+    
     int maximumCandies(vector<int>& candies, long long k) {
         int n=candies.size();
         long long sum=0;
         for(int i=0;i<n;i++)
             sum+=candies[i];
-        long long lo=0,hi=sum;
-        while(lo<hi)
+        long long lo=1,hi=sum;
+        int ans=0;
+        while(lo<=hi)
         {
-            long long mid=(lo+hi+1)/2,num=0;
+            long long mid=(lo+hi)/2,num=0;;
             for(int i=0;i<n && num<k;i++)
             {
                 num+=candies[i]/mid;
             }
             if(num>=k)
             {
-                lo=mid;
+                ans=mid;
+                lo=mid+1;
             }
             else
                 hi=mid-1;
         }
-        return lo;
+        return ans;
     }
 };
