993b64f5ca004e46beb1c0301985471da92fb47d
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-04-08 21:51:34 +0530

    Time: 357 ms (61.92%), Space: 101.6 MB (46.40%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
     int minSpeedOnTime(vector<int>& dist, double hour) {
-       long lo=1,hi;
-        double max_total=LONG_MIN;
-        int n=dist.size(),ans=-1;
+       // long lo=1,hi;
+       //  double max_total=LONG_MIN;
+       //  int n=dist.size(),ans=-1;
+       //  if(hour<n-1)
+       //      return -1;
+       //  for(int i=0;i<n;i++){
+       //      max_total=max(max_total,hour*dist[i]);
+       //  }
+       //  while(lo<=hi){
+       //      long mid=(lo+hi)/2;
+       //      double t=0;
+       //      for(int i=0;i<n;i++){
+       //          t=ceil(t);
+       //          t+=(double)dist[i]/mid;
+       //      }
+       //      if(t<=hour){
+       //          ans=mid;
+       //          // cout<<ans<<" ";
+       //          hi=mid-1;
+       //      } else{
+       //          lo=mid+1;
+       //      }
+       //  }
+       //  return ans;
+        
+        long lo=1,hi=1e7;
+       
+        int n=dist.size();
         if(hour<n-1)
             return -1;
-        for(int i=0;i<n;i++){
-            max_total=max(max_total,hour*dist[i]);
-        }
+       
         while(lo<=hi){
             long mid=(lo+hi)/2;
             double t=0;
@@ -17,13 +40,11 @@ public:
                 t+=(double)dist[i]/mid;
             }
             if(t<=hour){
-                ans=mid;
-                // cout<<ans<<" ";
                 hi=mid-1;
             } else{
                 lo=mid+1;
             }
         }
-        return ans;
+        return lo>1e7?-1:lo;
     }
 };
