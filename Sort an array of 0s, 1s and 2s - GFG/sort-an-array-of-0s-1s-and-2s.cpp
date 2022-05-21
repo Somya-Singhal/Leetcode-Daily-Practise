83b7809112e672116c7e61e1f10946a25f420f36
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-05-21 15:58:42 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
@@ -11,30 +11,40 @@ class Solution
     void sort012(int a[], int n)
     {
         // coode here
-        int arr[3]={0,0,0};
-        for(int i=0;i<n;i++)
-        {
-            if(a[i]==0)
-            arr[0]++;
-            else if(a[i]==1)
-            arr[1]++;
-            else if(a[i]==2)
-            arr[2]++;
-        }
-        int j=0,i=0;
-        while(i<n)
-        {
-            if(arr[j]==0)
-            j++;
-            else
-            {
-                a[i]=j;
-                arr[j]--;
-                i++;
-            }
-        }
+    //     int arr[3]={0,0,0};
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(a[i]==0)
+    //         arr[0]++;
+    //         else if(a[i]==1)
+    //         arr[1]++;
+    //         else if(a[i]==2)
+    //         arr[2]++;
+    //     }
+    //     int j=0,i=0;
+    //     while(i<n)
+    //     {
+    //         if(arr[j]==0)
+    //         j++;
+    //         else
+    //         {
+    //             a[i]=j;
+    //             arr[j]--;
+    //             i++;
+    //         }
+    //     }
+    // }
+       int lo=0,hi=n-1,mid=0;
+       while(mid<=hi)
+       {
+           if(a[mid]==0)
+           swap(a[lo++],a[mid++]);
+           else if(a[mid]==1)
+           mid++;
+           else
+           swap(a[hi--],a[mid]);
+       }
     }
-    
 };
 
 // { Driver Code Starts.
