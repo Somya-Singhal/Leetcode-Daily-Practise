71189407407452fd0ccdfdcae6bb5840c19cc963
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-06-10 18:11:45 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.cpp
+++ Power Set - GFG/power-set.cpp
@@ -5,24 +5,40 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 	public:
+	void solve(int i,string s,string str,vector<string>& res)
+	{
+	    if(i==s.length())
+	    {
+	        if(str!="")
+	        res.push_back(str);
+	        return;
+	    }
+	    
+	        solve(i+1,s,str+s[i],res);
+	        solve(i+1,s,str,res);
+	}
 		vector<string> AllPossibleStrings(string s){
 		    // Code here
 		    vector<string>res;
-		    int len=pow(2,s.length());
-		    for(int j=1;j<=len;j++){
-		        int temp=j;
-		        string ans="";
-		        for(int i=0;i<s.length() && temp>0;i++)
-		        {
-		            if(temp&1)
-		            ans+=s[i];
-		            temp=temp>>1;
-		        }
-		        if(ans!="")
-		        res.push_back(ans);
-		    }
-		    sort(res.begin(),res.end());
-		    return res;
+		  //  int len=pow(2,s.length());
+		  //  for(int j=1;j<=len;j++){
+		  //      int temp=j;
+		  //      string ans="";
+		  //      for(int i=0;i<s.length() && temp>0;i++)
+		  //      {
+		  //          if(temp&1)
+		  //          ans+=s[i];
+		  //          temp=temp>>1;
+		  //      }
+		  //      if(ans!="")
+		  //      res.push_back(ans);
+		  //  }
+		  //  sort(res.begin(),res.end());
+		  //  return res;
+		  
+		  solve(0,s,"",res);
+		  sort(res.begin(),res.end());
+		  return res;
 		}
 };
 
