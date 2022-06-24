9eb065eaf4570d69cacb8635ffc5ca537ad78a7d
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-06-24 13:24:28 +0530

    Added solution - LeetHub

--- Longest consecutive subsequence - GFG/longest-consecutive-subsequence.cpp
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.cpp
@@ -13,27 +13,55 @@ class Solution{
     int findLongestConseqSubseq(int arr[], int N)
     {
       //Your code here
-      unordered_set<int>s;
-      int maxcount=0;
-      for(int i=0;i<N;i++)
-      s.insert(arr[i]);
-      for(int i=0;i<N;i++)
-      {
-          int count=0;
-          if(s.find(arr[i]-1)==s.end())
-          {
-              count++;
-              int num=arr[i];
-              while(s.find(num+1)!=s.end())
-              {
-                  count++;
-                  num+=1;
-              }
+    //   unordered_set<int>s;
+    //   int maxcount=0;
+    //   for(int i=0;i<N;i++)
+    //   s.insert(arr[i]);
+    //   for(int i=0;i<N;i++)
+    //   {
+    //       int count=0;
+    //       if(s.find(arr[i]-1)==s.end())
+    //       {
+    //           count++;
+    //           int num=arr[i];
+    //           while(s.find(num+1)!=s.end())
+    //           {
+    //               count++;
+    //               num+=1;
+    //           }
               
-              maxcount=max(maxcount,count);
-          }
-      }
-      return maxcount;
+    //           maxcount=max(maxcount,count);
+    //       }
+    //   }
+    //   return maxcount;
+    
+    priority_queue<int,vector<int>,greater<int>>pq;
+    for(int i=0;i<N;i++)
+    pq.push(arr[i]);
+    int c=1,maxcount=1;
+    int prev=pq.top();
+    pq.pop();
+    while(!pq.empty())
+    {
+        if(pq.top()==prev+1)
+        {
+            c++;
+            prev=pq.top();
+            pq.pop();
+        }
+        else if(pq.top()-prev>0)
+        {
+            c=1;
+            prev=pq.top();
+            pq.pop();
+        }
+        else
+        {
+            pq.pop();
+        }
+        maxcount=max(maxcount,c);
+    }
+    return maxcount;
     }
 };
 
