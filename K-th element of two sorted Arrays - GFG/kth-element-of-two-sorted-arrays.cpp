dc51ae698ae3968c4ca8f1d355fbd832f519fe20
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-06-29 02:20:25 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,40 +8,25 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        int i=0,j=0;
-        while(i<n && j<m)
-        {
-
-            if(arr1[i]<=arr2[j])
-            {
-                k--;
-                if(k==0)
-                return arr1[i];
-                i++;
-            }
-            else {
-                k--;
-                if(k==0)
-                return arr2[j];
-                j++;
-            }
-            
-        }
-        while(i<n)
-        {
-            k--;
-            if(k==0)
-                return arr1[i];
-            i++;
-        }
-        while(j<m)
-        {
-                k--;
-                if(k==0)
-                return arr2[j];
-                j++;
-        }
-        return -1;
+        if(n>m)
+        return kthElement(arr2,arr1,m,n,k);
+    int l=max(0,k-m),h=min(k,n);
+    while(l<=h)
+    {
+        int mid1=l+(h-l)/2;
+        int mid2=k-mid1;
+        int l1=(mid1==0)?INT_MIN:arr1[mid1-1];
+        int l2=(mid2==0)?INT_MIN:arr2[mid2-1];
+        int r1=(mid1==n)?INT_MAX:arr1[mid1];
+        int r2=(mid2==m)?INT_MAX:arr2[mid2];
+        if(l1<=r2 && l2<=r1)
+        return max(l1,l2);
+        else if(l1>r2)
+        h=mid1-1;
+        else
+        l=mid1+1;
+    }
+    return -1;
     }
 };
 
