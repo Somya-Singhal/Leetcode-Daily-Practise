dc0cb81a7cab4619fd624eacab96812ec8e7f7a0
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-07-19 17:22:43 +0530

    Time: 2 ms (46.06%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    // vector<vector<int>> generate(int numRows) {
-    //     int dp[numRows][2*numRows-1];
-    //     vector<vector<int>>res;
-    //     for(int i=0;i<numRows;i++)
-    //     {
-    //         vector<int>v;
-    //         for(int j=numRows-i-1;j<=numRows+i-1;j+=2)
-    //         {
-    //             if(j==numRows-i-1 || j==numRows+i-1)
-    //             {
-    //                 dp[i][j]=1;
-    //                 v.push_back(dp[i][j]);
-    //             }
-    //             else
-    //             {
-    //                dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1];
-    //                 v.push_back(dp[i][j]);
-    //             }
-    //         }
-    //         res.push_back(v);
-    //     }
-    //     return res;
-    // }
-     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>res(numRows);
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>dp(numRows,vector<int>(2*numRows-1));
+        vector<vector<int>>res;
         for(int i=0;i<numRows;i++)
         {
-            res[i].resize(i+1);
-            res[i][0]=res[i][i]=1;
-            for(int j=1;j<i;j++)
+            vector<int>v;
+            for(int j=numRows-i-1;j<=numRows+i-1;j+=2)
             {
-                res[i][j]=res[i-1][j-1]+res[i-1][j];
+                if(j==numRows-i-1 || j==numRows+i-1)
+                {
+                    dp[i][j]=1;
+                    v.push_back(dp[i][j]);
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1];
+                    v.push_back(dp[i][j]);
+                }
             }
+            res.push_back(v);
         }
         return res;
     }
+    //  vector<vector<int>> generate(int numRows) {
+    //     vector<vector<int>>res(numRows);
+    //     for(int i=0;i<numRows;i++)
+    //     {
+    //         res[i].resize(i+1);
+    //         res[i][0]=res[i][i]=1;
+    //         for(int j=1;j<i;j++)
+    //         {
+    //             res[i][j]=res[i-1][j-1]+res[i-1][j];
+    //         }
+    //     }
+    //     return res;
+    // }
 };
