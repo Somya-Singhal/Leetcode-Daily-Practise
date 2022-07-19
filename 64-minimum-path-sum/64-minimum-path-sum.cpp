b7f8df32b3dcb7538a67de08ac3d5e24ed9972c6
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-07-19 18:42:22 +0530

    Time: 20 ms (25.88%), Space: 9.7 MB (90.08%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -20,33 +20,33 @@ public:
         // }
         // return grid[0][0];
         
+        // int m=grid.size(),n=grid[0].size();
+        // vector<int>pre(m,grid[m-1][n-1]),curr(m,0);
+        // for(int i=m-2;i>=0;i--)
+        //     pre[i]=pre[i+1]+grid[i][n-1];
+        // for(int j=n-2;j>=0;j--)
+        // {
+        //     curr[m-1]=pre[m-1]+grid[m-1][j];
+        //     for(int i=m-2;i>=0;i--)
+        //     {
+        //         curr[i]=min(curr[i+1],pre[i])+grid[i][j];
+        //     }
+        //     pre=curr;
+        // }
+        // return pre[0];
+        
         int m=grid.size(),n=grid[0].size();
-        vector<int>pre(m,grid[m-1][n-1]),curr(m,0);
+        vector<int>curr(m,grid[m-1][n-1]);
         for(int i=m-2;i>=0;i--)
-            pre[i]=pre[i+1]+grid[i][n-1];
+            curr[i]=curr[i+1]+grid[i][n-1];
         for(int j=n-2;j>=0;j--)
         {
-            curr[m-1]=pre[m-1]+grid[m-1][j];
+            curr[m-1]+=grid[m-1][j];
             for(int i=m-2;i>=0;i--)
             {
-                curr[i]=min(curr[i+1],pre[i])+grid[i][j];
+                curr[i]=min(curr[i+1],curr[i])+grid[i][j];
             }
-            pre=curr;
         }
-        return pre[0];
-        
-        // int m=grid.size(),n=grid[0].size();
-        // vector<int>curr(m,grid[0][0]);
-        // for(int i=1;i<m;i++)
-        //     curr[i]=curr[i-1]+grid[i][0];
-        // for(int j=1;j<n;j++)
-        // {
-        //     curr[0]+=grid[0][j];
-        //     for(int i=1;i<m;i++)
-        //     {
-        //         curr[i]=min(curr[i-1],curr[i])+grid[i][j];
-        //     }
-        // }
-        // return curr[m-1];
+        return curr[0];
     }
 };
