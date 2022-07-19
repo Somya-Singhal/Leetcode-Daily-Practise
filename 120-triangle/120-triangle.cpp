b15bbfb393c1382b34032de96f794d9b6240994b
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-07-19 19:44:21 +0530

    Time: 5 ms (85.94%), Space: 8.5 MB (88.92%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -24,27 +24,52 @@ public:
     //    }
     //     return ans;
     // }
+//      int minimumTotal(vector<vector<int>>& triangle) {
+//         int m=triangle.size();
+//         vector<int>pre(m,triangle[0][0]);
+//         vector<int>curr(m,0);
+//         for(int i=1;i<m;i++)
+//         {
+//             int n=triangle[i].size();
+//             curr[n-1]=pre[n-2]+triangle[i][n-1];
+//             curr[0]=pre[0]+triangle[i][0];
+            
+//             for(int j=1;j<=n-2;j++)
+//             {
+//                 curr[j]=min(pre[j-1],pre[j])+triangle[i][j];
+//             }
+//             pre=curr;
+//         }
+//          int ans=INT_MAX;
+//          for(int i=0;i<m;i++)
+//          {
+//              // cout<<curr[i]<<" ";
+//              ans=min(ans,pre[i]);
+//          }
+//          return ans;
+//     }
+    
      int minimumTotal(vector<vector<int>>& triangle) {
         int m=triangle.size();
-        vector<int>pre(m,triangle[0][0]);
-        vector<int>curr(m,0);
+        // vector<int>pre(m,triangle[0][0]);
+        vector<int>curr(m,triangle[0][0]);
         for(int i=1;i<m;i++)
         {
             int n=triangle[i].size();
-            curr[n-1]=pre[n-2]+triangle[i][n-1];
-            curr[0]=pre[0]+triangle[i][0];
+            curr[n-1]=curr[n-2]+triangle[i][n-1];
             
-            for(int j=1;j<=n-2;j++)
+            
+            for(int j=n-2;j>=1;j--)
             {
-                curr[j]=min(pre[j-1],pre[j])+triangle[i][j];
+                curr[j]=min(curr[j-1],curr[j])+triangle[i][j];
             }
-            pre=curr;
+            curr[0]+=triangle[i][0];
         }
          int ans=INT_MAX;
          for(int i=0;i<m;i++)
          {
              // cout<<curr[i]<<" ";
-             ans=min(ans,pre[i]);
+             ans=min(ans,curr[i]);
          }
          return ans;
     }
