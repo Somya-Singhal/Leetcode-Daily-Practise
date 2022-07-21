64ddcaa94bec3c5395bd4dc0bfb5964eb05259f8
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-07-21 09:52:34 +0530

    Time: 30 ms (44.68%), Space: 6.7 MB (42.81%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
-    int solve(int i,int copy,int n)
-    {
-        if(i>n)
-            return 1000000;
-        if(i==n)
-            return 0;
+//     int solve(int i,int copy,int n)
+//     {
+//         if(i>n)
+//             return 1000000;
+//         if(i==n)
+//             return 0;
         
-        return min(1+solve(i+copy,copy,n),2+solve(i+i,i,n));
-    }
+//         return min(1+solve(i+copy,copy,n),2+solve(i+i,i,n));
+//     }
+//     int minSteps(int n) {
+//         if(n==1)
+//             return 0;
+//         return 1+solve(1,1,n);
+//     }
+    
+    
     int minSteps(int n) {
-        if(n==1)
-            return 0;
-        return 1+solve(1,1,n);
+
+        vector<int>dp(n+1,0);
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=i;
+            for(int j=i-1;j>1;j--)
+            {
+                if(i%j==0)
+                {
+                    dp[i]=dp[j]+(i/j);
+                    break;
+                }
+                    
+            }
+        }
+        return dp[n];
     }
 };
