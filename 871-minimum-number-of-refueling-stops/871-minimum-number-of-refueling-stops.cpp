fe674732c8cd2eedc83e3397b24c221bbd6236cb
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-04 19:46:34 +0530

    Time: 176 ms (11.55%), Space: 16.2 MB (52.32%) - LeetHub

---
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+//     unordered_map<string,int>mp;
+//     int solve(int i,int prev,int target, int startFuel, vector<vector<int>>& stations)
+//     {
+//         int n=stations.size();
+//         if(i==n || startFuel<stations[i][0])
+//             return INT_MAX-1;
+//         if(target<=0)
+//             return 0;
+        
+//         string str=to_string(i)+" "+to_string(prev)+" "+to_string(target)+" "+to_string(startFuel);
+//         if(mp.find(str)!=mp.end())
+//             return mp[str];
+//         if(startFuel==stations[i][0])
+//             return mp[str]=1+solve(i+1,stations[i][0],target-prev,stations[i][1],stations);
+//         return mp[str]=min(1+solve(i+1,stations[i][0],target-prev,startFuel-prev+stations[i][1],stations),solve(i+1,prev,target,startFuel,stations));
+//     }
+//     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+//         int ans=solve(0,0,target,startFuel,stations);
+//         int n=stations.size();
+//         sort(stations.begin(),stations.end());
+        
+//         return ans==INT_MAX-1?-1:ans;
+//     }
+    
+    
+     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+       int n=stations.size();
+       vector<long long>dp(n+1,0);
+        dp[0]=startFuel;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(dp[j]>=stations[i][0])
+                    dp[j+1]=max(dp[j+1],dp[j]+stations[i][1]);
+            }
+        }
+        for(int i=0;i<=n;i++)
+        {
+            if(dp[i]>=target)
+                return i;
+        }
+         return -1;
+    }
+};
