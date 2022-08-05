a15421de553298fa2bc89e22841cc49dea930ca8
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-05 07:32:13 +0530

    Time: 4 ms (93.77%), Space: 9 MB (50.85%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
+    // int dp[201][201];
+    int solve(int row,int col,vector<vector<int>>& dp,vector<vector<int>>& dungeon)
+    {
+        int n=dungeon.size(),m=dungeon[0].size();
+        if(row==n || col==m)
+            return INT_MAX;
+        if(row==n-1 && col==m-1)
+            return (dungeon[row][col]<=0)?-dungeon[row][col]+1:1;
+        if(dp[row][col]!=INT_MAX)
+            return dp[row][col];
+        int right=solve(row,col+1,dp,dungeon);
+        int down=solve(row+1,col,dp,dungeon);
+        int ans=min(right,down)-dungeon[row][col];
+        // dp[row][col]=(ans<=0)?1:ans;
+        return dp[row][col]=(ans<=0)?1:ans;
+    }
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int n=dungeon.size();
-        int m=dungeon[0].size();
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=m-1;j>=0;j--)
-            {
-                if(i==n-1 && j==m-1)
-                {
-                    if(dungeon[i][j]>0)
-                        dungeon[i][j]=0;
-                }
-                else if(i==n-1)
-                {
-                    int temp=dungeon[i][j]+dungeon[i][j+1];
-                    if(temp>=0)
-                       dungeon[i][j]=0;
-                    else
-                      dungeon[i][j]=temp;  
-                }
-                else if(j==m-1)
-                {
-                    int temp=dungeon[i][j]+dungeon[i+1][j];
-                    if(temp>=0)
-                       dungeon[i][j]=0;
-                    else
-                      dungeon[i][j]=temp;  
-                }
-                else
-                {
-                       int temp=max(dungeon[i+1][j],dungeon[i][j+1]);
-                       if(dungeon[i][j]+temp>=0)
-                           dungeon[i][j]=0;
-                       else
-                           dungeon[i][j]+=temp;
-                }
-            }
-        }
-        return abs(dungeon[0][0])+1;
+        int n=dungeon.size(),m=dungeon[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
+        // memset(dp,INT_MAX,sizeof(dp));
+        return solve(0,0,dp,dungeon);
     }
 };
