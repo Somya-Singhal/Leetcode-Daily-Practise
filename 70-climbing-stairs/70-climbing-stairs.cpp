8509bb496e63ee76c28f56ace113dd21c2388839
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-06 10:13:03 +0530

    Time: 4 ms (22.32%), Space: 5.9 MB (56.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -12,11 +12,23 @@ public:
     //     return solve(n);
     // }
     
+    // int climbStairs(int n) {
+    //    vector<int>dp(n+1,0);
+    //    dp[0]=1,dp[1]=1;
+    //     for(int i=2;i<=n;i++)
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     return dp[n];
+    // }
+    
     int climbStairs(int n) {
-       vector<int>dp(n+1,0);
-       dp[0]=1,dp[1]=1;
+       int num1=1,num2=1;
+      
         for(int i=2;i<=n;i++)
-            dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        {
+            int curr=num1+num2;
+            num1=num2;
+            num2=curr;
+        }
+        return num2;
     }
 };
