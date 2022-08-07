5c2d47b1f05f60d6755d0db2a3e733404069d0df
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-07 12:59:46 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int target,vector<int>& memo)
-    {
-        if(target==0)
-            return 1;
-        if(target<0)
-            return 0;
-        if(memo[target]!=-1)
-            return memo[target];
-        int ways=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            ways+=solve(nums,target-nums[i],memo);
-        }
-        memo[target]=ways;
-        return memo[target];
-    }
+    // int solve(vector<int>& nums, int target,vector<int>& memo)
+    // {
+    //     if(target==0)
+    //         return 1;
+    //     if(target<0)
+    //         return 0;
+    //     if(memo[target]!=-1)
+    //         return memo[target];
+    //     int ways=0;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         ways+=solve(nums,target-nums[i],memo);
+    //     }
+    //     memo[target]=ways;
+    //     return memo[target];
+    // }
+    // int combinationSum4(vector<int>& nums, int target) {
+    //     vector<int>memo(target+1,-1);
+    //     return solve(nums,target,memo);
+    // }
+    
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>memo(target+1,-1);
-        return solve(nums,target,memo);
+        vector<int>memo(target+1,0);
+        memo[0]=1;
+        for(int i=1;i<=target;i++)
+        {
+            long ways=0;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(i>=nums[j])
+                     ways+=memo[i-nums[j]]; 
+            }
+            memo[i]=ways;  
+        }
+            
+        return memo[target];
     }
 };
