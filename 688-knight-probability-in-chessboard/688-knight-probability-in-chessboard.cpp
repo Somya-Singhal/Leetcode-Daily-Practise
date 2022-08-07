b22a2efcacd24da864f861ce108ea5431eb345d2
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-07 17:17:08 +0530

    Time: 8 ms (92.35%), Space: 6.6 MB (74.63%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int dx[8]={-2,-1,1,2,2,1,-1,-2},dy[8]={1,2,2,1,-1,-2,-2,-1};
-    unordered_map<string,double>mp;
+    
+    double dp[101][26][26];
     double solve(int n, int k, int x, int y)
     {
         if(x<0 || x>=n || y<0 || y>=n)
             return 0;
         if(k==0)
             return 1;
-        string str=to_string(k)+" "+to_string(x)+" "+to_string(y);
-        if(mp.find(str)!=mp.end())
-            return mp[str];
+        
+        if(dp[k][x][y]!=0)
+            return dp[k][x][y];
         double ways=0;
         for(int i=0;i<8;i++)
         {
             ways+=0.125*solve(n,k-1,x+dx[i],y+dy[i]);
         }
-        mp[str]=ways;
-        return mp[str];
+        dp[k][x][y]=ways;
+        return dp[k][x][y];
     }
     double knightProbability(int n, int k, int row, int column) {
+        memset(dp,0,sizeof(dp));
         return solve(n,k,row,column);
     }
 };
