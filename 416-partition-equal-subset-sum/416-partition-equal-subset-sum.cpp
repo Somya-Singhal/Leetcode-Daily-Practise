ad2ec668efe1b5b10eea9c10ea5f293058ead322
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-07 19:56:27 +0530

    Time: 337 ms (47.37%), Space: 9.6 MB (86.45%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -23,7 +23,33 @@ public:
     //     return solve(0,nums,sum/2);
     // }
     
-    bool canPartition(vector<int>& nums) {
+//     bool canPartition(vector<int>& nums) {
+//         int n=nums.size();
+//         int sum=0;
+//         for(int i=0;i<n;i++)
+//             sum+=nums[i];
+//         if(sum%2)
+//             return false;
+//         int target=sum/2;
+//         bool dp[n+1][target+1];
+//         for(int i=0;i<=target;i++)
+//             dp[0][i]=false;
+//         for(int i=0;i<=n;i++)
+//             dp[i][0]=true;
+        
+//         for(int i=1;i<=n;i++)
+//         {
+//             for(int j=1;j<=target;j++)
+//             {
+//                 dp[i][j]=dp[i-1][j];
+//                 if(j>=nums[i-1])
+//                     dp[i][j]=dp[i][j] || dp[i-1][j-nums[i-1]];
+//             }
+//         }
+//         return dp[n][target];
+//     }
+    
+     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
         for(int i=0;i<n;i++)
@@ -31,21 +57,17 @@ public:
         if(sum%2)
             return false;
         int target=sum/2;
-        bool dp[n+1][target+1];
-        for(int i=0;i<=target;i++)
-            dp[0][i]=false;
-        for(int i=0;i<=n;i++)
-            dp[i][0]=true;
+        vector<bool>dp(target+1,false);
+        dp[0]=true;
         
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=1;j<=target;j++)
+            for(int j=target;j>=1;j--)
             {
-                dp[i][j]=dp[i-1][j];
-                if(j>=nums[i-1])
-                    dp[i][j]=dp[i][j] || dp[i-1][j-nums[i-1]];
+                if(j>=nums[i])
+                    dp[j]=dp[j] || dp[j-nums[i]];
             }
         }
-        return dp[n][target];
+        return dp[target];
     }
 };
