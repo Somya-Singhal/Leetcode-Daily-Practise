798bc7d32de2d8aacb5dbcfd7f54056ce7e03dc5
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-07 15:38:39 +0530

    Time: 313 ms (34.16%), Space: 51.3 MB (16.34%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-    unordered_map<string,int>mp;
-    int solve(int i,vector<int>& nums,int target)
-    {
-        int n=nums.size();
-        if(i==n)
-            return target==0;
-       string str=to_string(i)+" "+to_string(target);
-       if(mp.find(str)!=mp.end())
-           return mp[str];
-        mp[str]=solve(i+1,nums,target+nums[i])+solve(i+1,nums,target-nums[i]);
-        return mp[str];
-    }
+    // unordered_map<string,int>mp;
+    // int solve(int i,vector<int>& nums,int target)
+    // {
+    //     int n=nums.size();
+    //     if(i==n)
+    //         return target==0;
+    //    string str=to_string(i)+" "+to_string(target);
+    //    if(mp.find(str)!=mp.end())
+    //        return mp[str];
+    //     mp[str]=solve(i+1,nums,target+nums[i])+solve(i+1,nums,target-nums[i]);
+    //     return mp[str];
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     int n=nums.size();
+    //     // vector<vector<int>>memo(n+1,vector<int>(1001,-1));
+    //     return solve(0,nums,target);
+    // }
+    
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        // vector<vector<int>>memo(n+1,vector<int>(1001,-1));
-        return solve(0,nums,target);
+        unordered_map<int,int>memo;
+        memo[0]=1;
+        for(int i=0;i<n;i++)
+        {
+            unordered_map<int,int>mp;
+            for(auto t: memo)
+            {
+                int num1=t.first+nums[i];
+                mp[num1]+=t.second;
+                int num2=t.first-nums[i];
+                mp[num2]+=t.second;
+            }
+            memo=mp;
+        }
+        return memo[target];
     }
 };
