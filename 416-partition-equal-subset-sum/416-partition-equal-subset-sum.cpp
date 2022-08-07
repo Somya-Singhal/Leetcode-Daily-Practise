9dc07df8d6098006462ce685fda59d68b41bb201
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-07 19:26:00 +0530

    Time: 279 ms (57.37%), Space: 11.7 MB (67.83%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    // int dp[201][10000];
+    // bool solve(int i,vector<int>& nums,int target)
+    // {
+    //     if(target==0)
+    //         return true;
+    //     if(target<0 || i==nums.size())
+    //         return false;
+    //     if(dp[i][target]!=0)
+    //         return dp[i][target];
+    //     dp[i][target]=solve(i+1,nums,target) || solve(i+1,nums,target-nums[i]);
+    //     return dp[i][target];
+    // }
+    // bool canPartition(vector<int>& nums) {
+    //     memset(dp,0,sizeof(dp));
+    //     int sum=0;
+    //     int n=nums.size();
+    //     for(int i=0;i<n;i++)
+    //         sum+=nums[i];
+    //     if(sum%2)
+    //         return false;
+    //     return solve(0,nums,sum/2);
+    // }
+    
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+            sum+=nums[i];
+        if(sum%2)
+            return false;
+        int target=sum/2;
+        bool dp[n+1][target+1];
+        for(int i=0;i<=target;i++)
+            dp[0][i]=false;
+        for(int i=0;i<=n;i++)
+            dp[i][0]=true;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                dp[i][j]=dp[i-1][j];
+                if(j>=nums[i-1])
+                    dp[i][j]=dp[i][j] || dp[i-1][j-nums[i-1]];
+            }
+        }
+        return dp[n][target];
+    }
+};
