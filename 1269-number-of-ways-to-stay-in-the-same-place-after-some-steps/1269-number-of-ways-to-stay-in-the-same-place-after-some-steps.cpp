335ff2499db2a65589b831279a41e40c63f7e721
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-11 07:18:17 +0530

    Time: 17 ms (87.93%), Space: 6.1 MB (95.37%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -2,31 +2,39 @@
 class Solution {
 public:
     int mod=1e9+7;
-    int memo[502][502];
-    int len;
-    int solve(int i,int steps)
-    {
-        if(steps==0 && i==0)
-            return i==0;
-        if(i<0 || i>=len || steps==0 || i>steps)
-            return 0;
-        if(memo[i][steps]!=-1)
-            return memo[i][steps];
-        return memo[i][steps]=((solve(i-1,steps-1)%mod+solve(i+1,steps-1))%mod+solve(i,steps-1))%mod;
+//     int memo[502][502];
+//     int len;
+//     int solve(int i,int steps)
+//     {
+//         if(steps==0 && i==0)
+//             return i==0;
+//         if(i<0 || i>=len || steps==0 || i>steps)
+//             return 0;
+//         if(memo[i][steps]!=-1)
+//             return memo[i][steps];
+//         return memo[i][steps]=((solve(i-1,steps-1)%mod+solve(i+1,steps-1))%mod+solve(i,steps-1))%mod;
         
-    }
-    int numWays(int steps, int arrLen) {
-        // memo.resize(steps,vector<int>(steps+1,-1));
-        memset(memo,-1,sizeof(memo));
-        len=arrLen;
-        return solve(0,steps);
-    }
-    
-//      int numWays(int steps, int arrLen) {
-        
+//     }
+//     int numWays(int steps, int arrLen) {
+//         memset(memo,-1,sizeof(memo));
 //         len=arrLen;
 //         return solve(0,steps);
 //     }
+    
+     int numWays(int steps, int arrLen) {
+         int len=min(steps/2+1,arrLen);
+       vector<int>v1(len+2),v2(len+2);
+       v1[1]=1;
+       while(steps-->0)
+       {
+           for(int j=1;j<=len;j++)
+           {
+               v2[j]=((v1[j-1]%mod+v1[j+1])%mod+v1[j])%mod;
+           }
+           swap(v1,v2);
+       }
+       return v1[1];
+    }
 };
 
     
