ef1fcc2541479ad657e28b6f1bc496c86cca125f
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-11 07:04:37 +0530

    Time: 13 ms (91.35%), Space: 7.1 MB (82.90%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
     int mod=1e9+7;
-    vector<vector<int>>memo;
+    int memo[502][502];
     int len;
     int solve(int i,int steps)
     {
@@ -16,34 +16,18 @@ public:
         
     }
     int numWays(int steps, int arrLen) {
-        memo.resize(steps/2+1,vector<int>(steps+1,-1));
+        // memo.resize(steps,vector<int>(steps+1,-1));
+        memset(memo,-1,sizeof(memo));
         len=arrLen;
         return solve(0,steps);
     }
-};
-
-// static int MOD=1e9+7;
-// class Solution {
-// public:
-//     vector<vector<int>> memo;
-//     int len;
-//     int solve(int i, int steps)
-//     {
-//         if(steps==0&&i==0)                                             //Base condition
-//             return 1;
-//         if(i<0||i>=len||steps==0||i>steps)					//Pruning.
-//             return 0;
-//         if(memo[i][steps]!=-1)         //If we have already cached the result for current `steps` and `index` get it.
-//             return memo[i][steps];
-//         return memo[i][steps]=((solve(i+1,steps-1)%MOD+dp(i-1,steps-1))%MOD+dp(i,steps-1))%MOD;        //Either move right, left or stay.
-            
-//     }
-//     int numWays(int steps, int arrLen) 
-//     {
-//         memo.resize(steps/2+1,vector<int>(steps+1,-1));
+    
+//      int numWays(int steps, int arrLen) {
+        
 //         len=arrLen;
 //         return solve(0,steps);
 //     }
-// };
+};
+
     
     
