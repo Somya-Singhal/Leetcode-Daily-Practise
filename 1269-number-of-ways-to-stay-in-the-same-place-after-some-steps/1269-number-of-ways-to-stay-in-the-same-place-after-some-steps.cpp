298c6b0190345ae3915fcda9077e7f7fce88759c
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-11 09:49:37 +0530

    Time: 38 ms (47.08%), Space: 10.2 MB (48.09%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -22,19 +22,35 @@ public:
 //     }
     
      int numWays(int steps, int arrLen) {
-         int len=min(steps/2+1,arrLen);
-       vector<int>v1(len+2),v2(len+2);
-       v1[1]=1;
-       while(steps-->0)
+       int len=min(steps/2+1,arrLen);
+       vector<vector<int>>dp(steps+1,vector<int>(len+2,0));
+       dp[0][1]=1;
+       for(int i=1;i<=steps;i++)
        {
            for(int j=1;j<=len;j++)
-           {
-               v2[j]=((v1[j-1]%mod+v1[j+1])%mod+v1[j])%mod;
-           }
-           swap(v1,v2);
+               dp[i][j]=((dp[i-1][j-1]%mod+dp[i-1][j+1])%mod+dp[i-1][j])%mod;
        }
-       return v1[1];
+         return dp[steps][1];
     }
+    
+    //  int numWays(int steps, int arrLen) {
+    //      int len=min(steps/2+1,arrLen);
+    //    vector<int>v1(len+2),v2(len+2);
+    //    v1[1]=1;
+    //    while(steps-->0)
+    //    {
+    //        for(int j=1;j<=len;j++)
+    //        {
+    //            v2[j]=((v1[j-1]%mod+v1[j+1])%mod+v1[j])%mod;
+    //        }
+    //        swap(v1,v2);
+    //    }
+    //      for(int i=0;i<=len+1;i++)
+    //          cout<<v1[i]<<" ";
+    //      for(int i=0;i<=len+1;i++)
+    //          cout<<v2[i]<<" ";
+    //    return v1[1];
+    // }
 };
 
     
