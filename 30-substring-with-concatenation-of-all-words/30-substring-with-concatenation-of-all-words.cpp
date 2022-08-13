7364a086628ae010c019e786438370e3435a2f69
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-13 07:00:06 +0530

    Time: 1568 ms (17.97%), Space: 286.4 MB (23.91%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        unordered_map<string,int>mp;
+        int n=words.size(),len=s.length(),wordSize=words[0].length();
+        vector<int>res;
+        vector<bool>vis(n,false);
+        for(int i=0;i<n;i++)
+            mp[words[i]]++;
+        for(int i=0;i<=len-n*wordSize;i++)
+        {
+            int j=i,count=0;
+            unordered_map<string,int>temp=mp;
+            while(true)
+            {
+                string str=s.substr(j,wordSize);
+                if(temp.find(str)!=temp.end())
+                {
+                    temp[str]--;
+                    if(temp[str]==0)
+                        temp.erase(str);
+                }
+                else
+                {
+                   
+                    break;
+                }
+                count+=1;
+                j+=wordSize;  
+            }
+            if(count==n)
+                res.push_back(i);
+        }
+        return res;
+    }
+};
