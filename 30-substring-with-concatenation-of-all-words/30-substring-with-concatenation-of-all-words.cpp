8a269407313eeffc6f95377b8a245b9609a5a5e9
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-13 07:05:27 +0530

    Time: 270 ms (69.97%), Space: 24.6 MB (60.09%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+//     vector<int> findSubstring(string s, vector<string>& words) {
+//         unordered_map<string,int>mp;
+//         int n=words.size(),len=s.length(),wordSize=words[0].length();
+//         vector<int>res;
+//         vector<bool>vis(n,false);
+//         for(int i=0;i<n;i++)
+//             mp[words[i]]++;
+//         for(int i=0;i<=len-n*wordSize;i++)
+//         {
+//             int j=i,count=0;
+//             unordered_map<string,int>temp=mp;
+//             while(true)
+//             {
+//                 string str=s.substr(j,wordSize);
+//                 if(temp.find(str)!=temp.end())
+//                 {
+//                     temp[str]--;
+//                     if(temp[str]==0)
+//                         temp.erase(str);
+//                 }
+//                 else
+//                 {
+                   
+//                     break;
+//                 }
+//                 count+=1;
+//                 j+=wordSize;  
+//             }
+//             if(count==n)
+//                 res.push_back(i);
+//         }
+//         return res;
+//     }
+    
     vector<int> findSubstring(string s, vector<string>& words) {
         unordered_map<string,int>mp;
         int n=words.size(),len=s.length(),wordSize=words[0].length();
@@ -9,26 +43,22 @@ public:
             mp[words[i]]++;
         for(int i=0;i<=len-n*wordSize;i++)
         {
-            int j=i,count=0;
-            unordered_map<string,int>temp=mp;
-            while(true)
+            int j;
+            unordered_map<string,int>vis;
+            for(j=0;j<n;j++)
             {
-                string str=s.substr(j,wordSize);
-                if(temp.find(str)!=temp.end())
+                string str=s.substr(i+j*wordSize,wordSize);
+                if(mp.find(str)!=mp.end())
                 {
-                    temp[str]--;
-                    if(temp[str]==0)
-                        temp.erase(str);
+                    vis[str]++;
+                    if(vis[str]>mp[str])
+                      break;
                 }
                 else
-                {
-                   
-                    break;
-                }
-                count+=1;
-                j+=wordSize;  
+                    break;       
             }
-            if(count==n)
+            
+            if(j==n)
                 res.push_back(i);
         }
         return res;
