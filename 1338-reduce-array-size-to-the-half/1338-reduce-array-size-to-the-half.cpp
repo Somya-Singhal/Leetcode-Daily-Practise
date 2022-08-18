9f398f193408a99cd374846d0f095469a0da0733
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-08-18 11:09:28 +0530

    Time: 286 ms (70.22%), Space: 78.3 MB (79.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-        map<int,int>mp;
-        priority_queue<pair<int,int>>pq;
+        // map<int,int>mp;
+        // priority_queue<pair<int,int>>pq;
+        // int n=arr.size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     mp[arr[i]]++;
+        // }
+        // for(auto x: mp)
+        // {
+        //     pq.push({x.second,x.first});
+        // }
+        // int sum=0;
+        // for(auto x: mp)
+        //     sum+=x.second;
+        // int target=sum/2,count=0;
+        // while(sum>target)
+        // {
+        //     sum-=pq.top().first;
+        //     pq.pop();
+        //     count+=1;
+        // }
+        // return count;
+        
+        unordered_map<int,int>mp;
         int n=arr.size();
         for(int i=0;i<n;i++)
         {
             mp[arr[i]]++;
         }
+        vector<int>freq;
+        int count=0;
         for(auto x: mp)
         {
-            pq.push({x.second,x.first});
+            freq.push_back(x.second);
         }
-        int sum=0;
-        for(auto x: mp)
-            sum+=x.second;
-        int target=sum/2,count=0;
-        while(sum>target)
+        sort(freq.begin(),freq.end(),greater<int>());
+        int i=0,target=n/2; 
+        while(n>target)
         {
-            sum-=pq.top().first;
-            pq.pop();
+            n-=freq[i++];
             count+=1;
         }
         return count;
