7539cae92e6e8f7af60eca276990de55c945ffa3
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-09-02 22:18:45 +0530

    Time: 15 ms (77.34%), Space: 8.2 MB (82.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    // int lengthOfLongestSubstring(string s) {
+    //     int ans=0;
+    //     int n=s.length();
+    //     unordered_map<char,int>lastInd;
+    //     int newstart=0;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(lastInd.find(s[i])!=lastInd.end())
+    //         {
+    //             ans=max(ans,i-max(lastInd[s[i]],newstart-1));
+    //             newstart=max(newstart-1,lastInd[s[i]])+1;
+    //         }
+    //         else
+    //             ans=max(ans,i-newstart+1);
+    //         lastInd[s[i]]=i;
+    //     }
+    //     return ans;
+    // }
+    
+     int lengthOfLongestSubstring(string s) {
         int ans=0;
         int n=s.length();
         unordered_map<char,int>lastInd;
@@ -8,13 +27,9 @@ public:
         for(int i=0;i<n;i++)
         {
             if(lastInd.find(s[i])!=lastInd.end())
-            {
-                ans=max(ans,i-max(lastInd[s[i]],newstart-1));
-                newstart=max(newstart-1,lastInd[s[i]])+1;
-            }
-            else
-                ans=max(ans,i-newstart+1);
+              newstart=max(newstart,lastInd[s[i]]+1);
             lastInd[s[i]]=i;
+            ans=max(ans,i-newstart+1);
         }
         return ans;
     }
