1153aa4168657380383ac58df8a46f6d500be178
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-09-29 14:06:52 +0530

    Time: 104 ms (53.88%), Space: 31.2 MB (86.01%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        map<int,multiset<int>>m;
-        vector<int>ans;
-        int n=arr.size();
-        for(int i=0;i<n;i++)
-        {
-            m[abs(arr[i]-x)].insert(arr[i]);
-        }
-        for(auto x: m)
-        {
-            if(ans.size()==k)
-                break;
-            for(auto y: x.second)
-            {
-                ans.push_back(y);
-                if(ans.size()==k)
-                    break;
-            }
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+    // vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    //     map<int,multiset<int>>m;
+    //     vector<int>ans;
+    //     int n=arr.size();
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         m[abs(arr[i]-x)].insert(arr[i]);
+    //     }
+    //     for(auto x: m)
+    //     {
+    //         if(ans.size()==k)
+    //             break;
+    //         for(auto y: x.second)
+    //         {
+    //             ans.push_back(y);
+    //             if(ans.size()==k)
+    //                 break;
+    //         }
+    //     }
+    //     sort(ans.begin(),ans.end());
+    //     return ans;
+    // }
+    
+     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+       int n=arr.size();
+       int left=0,right=n-k;
+       while(left<right)
+       {
+           int mid=(left+right)/2;
+           if(x-arr[mid]>arr[mid+k]-x)
+               left=mid+1;
+           else
+               right=mid;
+       }
+      return vector<int>(arr.begin()+left,arr.begin()+left+k);
+         
     }
 };
