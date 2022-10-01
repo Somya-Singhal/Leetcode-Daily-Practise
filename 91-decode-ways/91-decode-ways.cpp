c322fbc7e0157c73ce23bd1eaf729c9710ce4be8
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-10-01 14:28:56 +0530

    Time: 2 ms (67.17%), Space: 6.2 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,37 +1,58 @@
 class Solution {
 public:
-    int solve(int s,string str,vector<int>& dp)
-    {
+    // int solve(int s,string str,vector<int>& dp)
+    // {
+    //     int n=str.length();
+    //     if(s==n)
+    //         return 1;
+    //     if(str[s]=='0')
+    //         return 0;
+    //     if(dp[s]!=0)
+    //         return dp[s];
+    //     int count=0;
+    //     if(str[s]>'0' && str[s]<='9')
+    //         count+=solve(s+1,str,dp);
+    //     if(s<n-1 && (str[s]=='1' || (str[s]=='2' && str[s+1]<'7')))
+    //         count+=solve(s+2,str,dp);
+    
+    //*******this code worked but giving TLE  ***** //
+    //     // for(int k=s;k<n;k++)
+    //     // {
+    //     //     if(k-s>1)
+    //     //         break;
+    //     //     stringstream ss;
+    //     //     string sub=str.substr(s,k-s+1);
+    //     //     int num;
+    //     //     ss<<sub;
+    //     //     ss>>num;
+    //     //     if(num>0 && num<27)
+    //     //         count+=solve(k+1,str,dp);
+    //     // }
+    //*******          ***********//
+    //     dp[s]=count;
+    //     return dp[s];
+    // }
+    // int numDecodings(string s) {
+    //     int n=s.length();
+    //     vector<int>dp(n+1,0);
+    //    return solve(0,s,dp);
+    // }
+    
+     int numDecodings(string str) {
         int n=str.length();
-        if(s==n)
-            return 1;
-        if(str[s]=='0')
-            return 0;
-        if(dp[s]!=0)
-            return dp[s];
-        int count=0;
-        if(str[s]>'0' && str[s]<='9')
-            count+=solve(s+1,str,dp);
-        if(s<n-1 && (str[s]=='1' || (str[s]=='2' && str[s+1]<'7')))
-            count+=solve(s+2,str,dp);
-        // for(int k=s;k<n;k++)
-        // {
-        //     if(k-s>1)
-        //         break;
-        //     stringstream ss;
-        //     string sub=str.substr(s,k-s+1);
-        //     int num;
-        //     ss<<sub;
-        //     ss>>num;
-        //     if(num>0 && num<27)
-        //         count+=solve(k+1,str,dp);
-        // }
-        dp[s]=count;
-        return dp[s];
-    }
-    int numDecodings(string s) {
-        int n=s.length();
         vector<int>dp(n+1,0);
-       return solve(0,s,dp);
+        dp[n]=1;
+        for(int s=n-1;s>=0;s--)
+        {
+            if(str[s]=='0')
+              continue;
+            int count=0;
+            if(str[s]>'0' && str[s]<='9')
+                count+=dp[s+1];
+            if(s<n-1 && (str[s]=='1' || (str[s]=='2' && str[s+1]<'7')))
+                count+=dp[s+2];
+            dp[s]=count;    
+        }
+       return dp[0];
     }
 };
