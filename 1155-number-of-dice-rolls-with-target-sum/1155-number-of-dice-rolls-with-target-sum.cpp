340e807e82f4bd1685a5b5564eb022be7f76dc70
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2022-10-02 21:12:30 +0530

    Time: 31 ms (74.07%), Space: 8.4 MB (59.18%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,58 +1,24 @@
 class Solution {
 public:
     int mod=1e9+7;
-    
-//     int solve(int i,int sum,int n,int k,int target,vector<vector<int>>& dp)
-//     {
-//         if(i==n)
-//             return (sum==target);
-        
-//         if(dp[i][sum]!=-1)
-//             return dp[i][sum];
-//         int ways=0;
-//         for(int j=1;j<=min(k,target-sum);j++)
-//         {
-//             ways=(ways+solve(i+1,sum+j,n,k,target,dp))%mod;
-//         }
-//         return dp[i][sum]=ways;
-//     }
-//     int numRollsToTarget(int n, int k, int target) {
-//         vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-        
-//         return solve(0,0,n,k,target,dp);
-//     }
-    
-    // int numRollsToTarget(int n, int k, int target) {
-    //     vector<vector<int>>dp(n+1,vector<int>(target+1,0));
-    //     dp[n][target]=1;
-    //     for(int i=n-1;i>=0;i--)
-    //     {
-    //         for(int j=0;j<=target;j++)
-    //         {
-    //             int ways=0;
-    //             for(int t=1;t<=min(k,target-j);t++)
-    //                 ways=(ways+dp[i+1][j+t])%mod;
-    //             dp[i][j]=ways;
-    //         }
-    //     }
-    //     return dp[0][0];
-    // }
-    
-    
-    int solve(int d,int k,int target,vector<vector<int>>& memo)
+    int solve(int n,int k,int target,vector<vector<int>>& dp)
     {
-        if(d==0)
-            return target==0;
-        if(memo[d][target]!=-1)
-            return memo[d][target];
-        int ways=0;
-        for(int i=max(0,target-k);i<target;i++)
-            ways=(ways+solve(d-1,k,i,memo))%mod;
-        memo[d][target]=ways;
-        return memo[d][target];
+        if(n==0 && target==0)
+            return 1;
+        if(target<0 || n<0)
+            return 0;
+        if(dp[n][target]!=-1)
+            return dp[n][target];
+        int count=0;
+        for(int m=1;m<=k;m++)
+        {
+            count=(count+solve(n-1,k,target-m,dp))%mod;
+        }
+        dp[n][target]=count;
+        return dp[n][target];
     }
-     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>memo(n+1,vector<int>(target+1,-1));
-        return solve(n,k,target,memo)%mod;
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        return solve(n,k,target,dp);
     }
 };
