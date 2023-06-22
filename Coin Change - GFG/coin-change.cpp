7ac585656d150989e3d5ba8611dab5390fc49363
Somya Singhal <88088520+Somya-Singhal@users.noreply.github.com>
2023-06-23 00:46:28 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -1,58 +1,46 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
   public:
-//   long long solve(int i,int a[],int n,int s)
-//   {
-//       if(s==0)
-//       return 1;
-//       if(i==n)
-//       return 0;
-//       if(a[i]<=s)
-//       return solve(i,a,n,s-a[i])+solve(i+1,a,n,s);
-//       return solve(i+1,a,n,s);
-//   }
-    long long int count(int S[], int m, int n) {
+
+    long long int count(int coins[], int N, int sum) {
 
         // code here.
-        vector<vector<long long>>dp(m+1,vector<long long>(n+1,0));
-        for(int i=0;i<=m;i++)
+        vector<vector<long long int>>dp(N+1,vector<long long int>(sum+1,0));
+        for(int i=0;i<=N;i++)
         dp[i][0]=1;
-        for(int i=1;i<=m;i++)
+        for(int i=1;i<=N;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j=1;j<=sum;j++)
             {
-                if(S[i-1]<=j)
-                {
-                    dp[i][j]=dp[i][j-S[i-1]]+dp[i-1][j];
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
+                dp[i][j]=dp[i-1][j];
+                if(coins[i-1]<=j)
+                dp[i][j]+=dp[i][j-coins[i-1]];
+                
             }
         }
-        return dp[m][n];
-        // vector<vector<int>>dp(m,vector<int>(n+1,0))
-        // return solve(0,S,m,n);
+        return dp[N][sum];
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, m;
-        cin >> n >> m;
-        int arr[m];
-        for (int i = 0; i < m; i++) cin >> arr[i];
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
         Solution ob;
-        cout << ob.count(arr, m, n) << endl;
+        cout << ob.count(coins, N, sum) << endl;
     }
 
     return 0;
-}  // } Driver Code Ends
+}
+
+
+// } Driver Code Ends
